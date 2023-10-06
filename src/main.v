3b96dc15da3885efcf35285e65a8b90326969469
tauraamui <adampstringer@protonmail.com>
2023-10-06 23:04:53 +0100

    implement cursor line and up/down movement

--- src/main.v
+++ src/main.v
@@ -2,18 +2,33 @@ module main
 
 import term.ui as tui
 
+enum Mode as u8 {
+	normal
+	visual
+	insert
+}
+
 struct Pos {
+mut:
 	x int
 	y int
 }
 
 struct Cursor {
+mut:
 	pos Pos
+}
+
+fn (cursor Cursor) draw(mut ctx tui.Context) {
+    ctx.set_bg_color(r: 53, g: 53, b: 53)
+	ctx.draw_rect(0, cursor.pos.y, ctx.window_width - 1, cursor.pos.y)
+
 }
 
 struct App {
 mut:
     tui    &tui.Context = unsafe { nil }
+	mode   Mode
 	cursor Cursor
 }
 
@@ -21,13 +36,20 @@ fn event(e &tui.Event, mut app &App) {
     if e.typ == .key_down && e.code == .escape {
         exit(0)
     }
+
+	if e.typ == .key_down {
+		match e.code {
+			.j { app.cursor.pos.y += 1 }
+			.k { app.cursor.pos.y -= 1; if app.cursor.pos.y < 1 { app.cursor.pos.y = 1 } }
+			else {}
+		}
+	}
 }
 
 fn frame(mut app &App) {
     app.tui.clear()
 
-    app.tui.set_bg_color(r: 53, g: 53, b: 53)
-	app.tui.draw_rect(0, app.cursor.pos.y, app.tui.window_width - 1, app.cursor.pos.y)
+	app.cursor.draw(mut app.tui)
 
     app.tui.reset()
     app.tui.flush()
@@ -35,14 +57,15 @@ fn frame(mut app &App) {
 
 fn main() {
     mut app := &App{
-		cursor: Cursor{ pos: Pos{ x: 0, y: 0 } }
+		cursor: Cursor{ pos: Pos{ x: 1, y: 1 } }
+		mode: .normal
 	}
     app.tui = tui.init(
         user_data: app
         event_fn: event
         frame_fn: frame
         hide_cursor: true
-		capture_events: false
+		capture_events: true
     )
     app.tui.run()!
 }
