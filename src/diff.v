b0ba0151aba7cc2dedc6097af124ce4df3974767
Adam Stringer <adampstringer@protonmail.com>
2023-11-03 17:28:20 +0000

    Util/implement diff algo (#50)
    * define diff implementation
    
    * define basic tests to ensure ops are generated correctly

---
+++ src/diff.v
@@ -0,0 +1,196 @@
+module main
+
+import arrays
+import math
+
+fn same(left []string, right []string) bool {
+	if left.len != right.len { return false }
+	for idx, cur in left {
+		if right[idx] != cur { return false }
+	}
+	return true
+}
+
+struct Op {
+	kind  string
+	value string
+}
+
+struct Entry {
+mut:
+	value string
+	ref   int
+	count int
+	eof   bool
+}
+
+fn add_to_table(mut table map[string]map[int]map[string]int, arr []Entry, kind string) {
+	arrays.each_indexed[Entry](arr, fn [mut table, kind] (idx int, token Entry) {
+		if !(token.value in table) { table[token.value] = map[int]map[string]int{} }
+		if !(token.count in table[token.value]) { table[token.value][token.count] = { "left": -1, "right": -1 } }
+		v := table[token.value][token.count][kind]
+		if v == -1 { table[token.value][token.count][kind] = idx } else if v >= 0 { table[token.value][token.count][kind] = -2 }
+	})
+}
+
+fn find_unique(mut table map[string]map[int]map[string]int, mut left []Entry, mut right []Entry) {
+	for token in left {
+		ref := table[token.value][token.count].clone()
+		if ref["left"] >= 0 && ref["right"] >= 0 {
+			left_token := left[ref["left"]]
+			right_token := right[ref["right"]]
+			left[ref["left"]] = Entry{ value: left_token.value, ref: ref["right"], count: left_token.count }
+			right[ref["right"]] = Entry{ value: right_token.value, ref: ref["left"], count: right_token.count }
+		}
+	}
+}
+
+fn expand_unique(mut left []Entry, mut right []Entry, dir int) {
+	for idx, token in left {
+		if token.ref == -1 { return }
+		mut i := idx + dir
+		mut j := token.ref + dir
+		lx := left.len
+		rx := right.len
+
+		for i >= 0 && j >= 0 && i < lx && j < rx {
+			if left[i].value != right[j].value { break }
+		}
+
+		left[i].ref = j
+		right[j].ref = i
+
+		i += dir
+		j += dir
+	}
+}
+
+fn append_multiple(mut acc []Op, token Entry, kind string) {
+	mut n := token.count
+	for _ in 0..n {
+		acc << Op{ kind: kind, value: token.value }
+	}
+}
+
+fn calc_dist(l_target int, l_pos int, r_target int, r_pos int) int {
+	return (l_target - l_pos) + (r_target - r_pos) + math.abs((l_target - l_pos) - (r_target - r_pos))
+}
+
+fn process_diff(left []Entry, right []Entry) []Op {
+	mut acc := []Op{}
+	mut l_pos := 0
+	mut r_pos := 0
+	lx := left.len
+	rx := right.len
+	mut l_token := Entry{}
+	mut r_token := Entry{}
+	mut l_target := 0
+	mut r_target := 0
+
+	for l_pos < lx {
+		l_target = l_pos
+
+		for left[l_target].ref < 0 {
+			l_target += 1
+		}
+
+		r_target = left[l_target].ref
+
+		if r_target < r_pos {
+			for l_pos < l_target {
+				append_multiple(mut acc, left[l_pos], "del")
+			}
+
+			append_multiple(mut acc, left[l_pos++], "del")
+			continue
+		}
+
+		r_token = right[r_target - 1]
+
+		mut dist_1 := calc_dist(l_target, l_pos, r_target, r_pos)
+
+		for r_seek := r_target - 1; dist_1 > 0 && r_seek >= r_pos; r_seek-- {
+			if right[r_seek].ref < 0 { continue }
+			if right[r_seek].ref < l_pos { continue }
+
+			mut dist_2 := calc_dist(right[r_seek].ref, l_pos, r_seek, r_pos)
+			if dist_2 < dist_1 {
+				dist_1 = dist_2
+				r_target = r_seek
+				l_target = right[r_seek].ref
+			}
+		}
+
+		for l_pos < l_target {
+			append_multiple(mut acc, left[l_pos++], "del")
+		}
+
+		for r_pos < r_target {
+			append_multiple(mut acc, right[r_pos++], "ins")
+		}
+
+		if left[l_pos].eof { break }
+
+		count_diff := left[l_pos].count - right[r_pos].count
+
+		if count_diff == 0 {
+			append_multiple(mut acc, left[l_pos], "same")
+		} else if count_diff < 0 {
+			append_multiple(mut acc, Entry{ count: right[r_pos].count + count_diff, value: right[r_pos].value }, "same")
+			append_multiple(mut acc, Entry{ count: -count_diff, value: right[r_pos].value }, "ins")
+		} else if count_diff > 0 {
+			append_multiple(mut acc, Entry{ count: left[l_pos].count - count_diff, value: left[l_pos].value }, "same")
+			append_multiple(mut acc, Entry{ count: count_diff, value: left[l_pos].value }, "del")
+		}
+
+		l_pos += 1
+		r_pos += 1
+	}
+	return acc
+}
+
+fn diff(a []string, b []string) []Op {
+	if same(a, b) { return a.map(fn (v string) Op { return Op{ kind: "same", value: v } })}
+	if a.len == 0 { return b.map(fn (v string) Op { return Op{ kind: "ins", value: v } }) }
+	if b.len == 0 { return a.map(fn (v string) Op { return Op{ kind: "del", value: v } }) }
+
+	mut left := []Entry{}
+	for i, cur in a {
+		if left.len > 0 && left[left.len - 1].value == cur {
+			left[left.len - 1].count += 1
+			continue
+		}
+		left << Entry{
+			value: cur,
+			ref: -1,
+			count: 1
+		}
+	}
+
+	mut right := []Entry{}
+	for i, cur in b {
+		if right.len > 0 && right[right.len - 1].value == cur {
+			right[right.len - 1].count += 1
+			continue
+		}
+		right << Entry{
+			value: cur,
+			ref: -1,
+			count: 1
+		}
+	}
+
+	mut table := map[string]map[int]map[string]int{}
+	add_to_table(mut table, left, "left")
+	add_to_table(mut table, right, "right")
+
+	find_unique(mut table, mut left, mut right)
+
+	expand_unique(mut left, mut right, 1)
+	expand_unique(mut left, mut right, -1)
+
+	left << Entry{ ref: right.len, eof: true }
+
+	return process_diff(left, right)
+}
+
---
+++ src/diff_test.v
@@ -0,0 +1,56 @@
+module main
+
+fn test_same_check_is_matching() {
+	assert same(["a", "b", "c"], ["a", "b", "c"])
+}
+
+fn test_same_check_is_different() {
+	assert !same(["bb", "c", "f"], ["a", "b", "c"])
+}
+
+fn test_diff_same() {
+	ops := diff(["a", "b", "c"], ["a", "b", "c"])
+	assert ops == [Op{kind: "same", value: "a"}, Op{kind: "same", value: "b"}, Op{kind: "same", value: "c"}]
+}
+
+fn test_add_to_table() {
+	mut table := map[string]map[int]map[string]int{}
+
+	mut left_entries := [Entry{value: "a", ref: -1, count: 1}]
+	mut right_entries := [Entry{value: "a", ref: -1, count: 1}, Entry{value: "b", ref: -1, count: 1}]
+
+	add_to_table(mut table, left_entries, "left")
+	add_to_table(mut table, right_entries, "right")
+
+	assert table == {'a': {1: {'left': 0, 'right': 0}}, 'b': {1: {'left': -1, 'right': 1}}}
+}
+
+fn test_diff_left_empty_right_not() {
+	ops := diff([], ["a", "b", "c"])
+	assert ops == [Op{kind: "ins", value: "a"}, Op{kind: "ins", value: "b"}, Op{kind: "ins", value: "c"}]
+}
+
+fn test_diff_right_empty_left_not() {
+	ops := diff(["a", "b", "c"], [])
+	assert ops == [Op{kind: "del", value: "a"}, Op{kind: "del", value: "b"}, Op{kind: "del", value: "c"}]
+}
+
+fn test_diff_single_char() {
+	ops := diff(["x"], ["s"])
+	assert ops == [
+		Op{kind: "del", value: "x"},
+		Op{kind: "ins", value: "s"}
+	]
+}
+
+fn test_diff_left_and_right() {
+	ops := diff(["c", "c", "b", "c", "d"], ["a", "b", "c"])
+	assert ops == [
+		Op{kind: "del", value: "c"},
+		Op{kind: "del", value: "c"},
+		Op{kind: "ins", value: "a"},
+		Op{kind: "same", value: "b"},
+		Op{kind: "same", value: "c"},
+		Op{kind: "del", value: "d"}
+	]
+}
