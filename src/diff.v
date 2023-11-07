225474e603e755f4b319524117c75d139289136d
tauraamui <adampstringer@protonmail.com>
2023-11-07 10:16:47 +0000

    address PR comments and include license header

--- src/diff.v
+++ src/diff.v
@@ -1,3 +1,17 @@
+// Copyright 2023 The Lilly Editor contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 module main
 
 import arrays
@@ -70,7 +84,7 @@ fn append_multiple(mut acc []Op, token Entry, kind string) {
 	mut n := token.count
 	for n > 0 {
 		acc << Op{ kind: kind, value: token.value }
-		n--
+		n -= 1
 	}
 }
 
@@ -144,7 +158,7 @@ fn process_diff(left []Entry, right []Entry) []Op {
 		l_target = l_pos
 
 		for left[l_target].ref < 0 {
-			l_target++
+			l_target += 1
 		}
 
 		r_target = left[l_target].ref
