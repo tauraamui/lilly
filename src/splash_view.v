4fabdd47c90191437e7f342f21d95ecd58a5156e
tauraamui <adampstringer@protonmail.com>
2023-11-15 19:21:29 +0000

    draw editor name/title underneath splash logo

--- src/splash_view.v
+++ src/splash_view.v
@@ -1,6 +1,7 @@
 module main
 
 import term.ui as tui
+import math
 
 const logo_contents = $embed_file("./src/splash-logo.txt")
 
@@ -31,13 +32,16 @@ pub fn new_splash() SplashScreen {
 
 pub fn (splash SplashScreen) draw(mut ctx tui.Context) {
 	offset_x := 1
+	mut offset_y := 1 + f64(ctx.window_height) * 0.1
 	for i, l in splash.logo.data {
-		ctx.draw_text(offset_x+(ctx.window_width / 2) - (l.runes().len / 2), i+1, l)
+		ctx.draw_text(offset_x+(ctx.window_width / 2) - (l.runes().len / 2), int(math.floor(offset_y))+i, l)
 	}
 
-	for x in 0..ctx.window_height {
-		ctx.draw_text(offset_x + ctx.window_width / 2, x+1, "|")
-	}
+	offset_y += splash.logo.data.len
+	offset_y += (ctx.window_height - offset_y) * 0.05
+
+	the_lilly_editor := "The Lilly Editor"
+	ctx.draw_text(offset_x+(ctx.window_width / 2) - (the_lilly_editor.len / 2), int(math.floor(offset_y)), the_lilly_editor)
 }
 
 pub fn (splash SplashScreen) on_key_down(e &tui.Event) {
