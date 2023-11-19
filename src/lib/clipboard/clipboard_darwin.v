4bdba719f9807032a841b712d616f0c4cf0461c1
tauraamui <adampstringer@protonmail.com>
2023-11-19 21:00:22 +0000

    mix silly syntax mistake

--- src/lib/clipboard/clipboard_darwin.v
+++ src/lib/clipboard/clipboard_darwin.v
@@ -3,7 +3,7 @@ module clipboard
 import clipboard as stdlib_clipboard
 import os
 
-fn new_clipboard() Clipboard() {
+fn new_clipboard() Clipboard {
 	return StdLibClipboard{ ref: stdlib_clipboard.new() }
 }
 
