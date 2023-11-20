e6190885dc091b310290ac8c01acdfe3e5f2cd29
tauraamui <adampstringer@protonmail.com>
2023-11-20 07:26:51 +0000

    define browser max height and prevent file list render overflow

--- src/file_finder_modal.v
+++ src/file_finder_modal.v
@@ -16,23 +16,23 @@ module main
 
 import term.ui as tui
 
+const max_height = 20
+
 struct FileFinderModal {
 pub:
 	file_paths []string
-	max_height int
 	from       int
 mut:
 	current_selection int
 }
 
-fn (file_finder_modal FileFinderModal) draw(mut ctx tui.Context) {
+fn (mut file_finder_modal FileFinderModal) draw(mut ctx tui.Context) {
 	defer { ctx.reset_bg_color() }
 	ctx.set_color(r: 245, g: 245, b: 245)
 	ctx.set_bg_color(r: 15, g: 15, b: 15)
-	ctx.draw_text(1, 1, "WORKSPACE FILES")
-	for i, l in file_finder_modal.file_paths.filter(fn (it string) bool {
-		return !it.starts_with("./.git")
-	}) {
+	ctx.draw_text(1, 1, "=== FILE BROWSER ===")
+	/*
+	for i, l in file_finder_modal.file_paths {
 		ctx.reset_bg_color()
 		ctx.set_bg_color(r: 15, g: 15, b: 15)
 		if i == file_finder_modal.current_selection {
@@ -41,7 +41,25 @@ fn (file_finder_modal FileFinderModal) draw(mut ctx tui.Context) {
 		ctx.draw_rect(1, i+2, ctx.window_width, i+2)
 		ctx.draw_text(1, i+2, l)
 	}
+	*/
+	file_finder_modal.draw_scrollable_list(mut ctx, 2, file_finder_modal.file_paths)
 	ctx.set_cursor_position(1, file_finder_modal.current_selection + 2)
+}
+
+fn (mut file_finder_modal FileFinderModal) draw_scrollable_list(mut ctx tui.Context, y_offset int, list []string) {
+	ctx.reset_bg_color()
+	ctx.set_bg_color(r: 15, g: 15, b: 15)
+	ctx.draw_rect(1, y_offset, ctx.window_width, y_offset+max_height)
+	for i := 0; i < list.len; i++ {
+		if i > max_height { continue }
+		if i == file_finder_modal.current_selection {
+			ctx.set_bg_color(r: 53, g: 53, b: 53)
+			ctx.draw_text(1, y_offset+i, list[i])
+			ctx.reset_bg_color()
+			continue
+		}
+		ctx.draw_text(1, y_offset+i, list[i])
+	}
 }
 
 fn (mut file_finder_modal FileFinderModal) on_key_down(e &tui.Event, mut root Root) {
