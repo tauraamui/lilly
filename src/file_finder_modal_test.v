6241fc37b33ce9d3a946f465c2c78e4f57dd4e8a
tauraamui <adampstringer@protonmail.com>
2023-11-24 22:46:05 +0000

    fixtests: ensure ordering and searching seems correct

--- src/file_finder_modal_test.v
+++ src/file_finder_modal_test.v
@@ -10,12 +10,14 @@ fn test_resolve_file_paths_returns_realistic_results() {
 
 	mock_modal.search.query = "some"
 	assert mock_modal.resolve_file_paths() == [
-		"./src/project/lib/some_utilities.v"
+		"./src/project/lib/some_utilities.v",
+		"./src/project/main.v"
 	]
 
 	mock_modal.search.query = "mai"
 	assert mock_modal.resolve_file_paths() == [
 		"./src/project/main.v",
+		"./src/project/lib/some_utilities.v",
 	]
 
 	mock_modal.search.query = "proj"
@@ -31,7 +33,10 @@ fn test_resolve_file_paths_returns_realistic_results() {
 	]
 
 	mock_modal.search.query = "zkf"
-	assert mock_modal.resolve_file_paths() == []
+	assert mock_modal.resolve_file_paths() == [
+		"./src/project/lib/some_utilities.v",
+		"./src/project/main.v"
+	]
 }
 
 fn test_fuzzy_searching_is_operational() {
@@ -44,7 +49,8 @@ fn test_fuzzy_searching_is_operational() {
 
 	mock_modal.search.query = "ut"
 	assert mock_modal.resolve_file_paths() == [
-		"./src/project/lib/some_utilities.v"
+		"./src/project/lib/some_utilities.v",
+		"./src/project/main.v"
 	]
 }
 
