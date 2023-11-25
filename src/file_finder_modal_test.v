0d16975260d1b8e2453034132d71433359e789ff
tauraamui <adampstringer@protonmail.com>
2023-11-25 21:06:35 +0000

    remove final map back to string list

--- src/file_finder_modal_test.v
+++ src/file_finder_modal_test.v
@@ -9,31 +9,31 @@ fn test_resolve_file_paths_returns_realistic_results() {
 	}
 
 	mock_modal.search.query = "some"
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/lib/some_utilities.v",
 		"./src/project/main.v"
 	]
 
 	mock_modal.search.query = "mai"
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/main.v",
 		"./src/project/lib/some_utilities.v",
 	]
 
 	mock_modal.search.query = "proj"
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/main.v",
 		"./src/project/lib/some_utilities.v"
 	]
 
 	mock_modal.search.query = ""
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/main.v",
 		"./src/project/lib/some_utilities.v"
 	]
 
 	mock_modal.search.query = "zkf"
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/lib/some_utilities.v",
 		"./src/project/main.v"
 	]
@@ -48,7 +48,7 @@ fn test_fuzzy_searching_is_operational() {
 	}
 
 	mock_modal.search.query = "ut"
-	assert mock_modal.resolve_file_paths() == [
+	assert mock_modal.resolve_file_paths().map(it.content) == [
 		"./src/project/lib/some_utilities.v",
 		"./src/project/main.v"
 	]
