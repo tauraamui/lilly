c9106239951f7d41c665ecf45c7172ad847e282e
tauraamui <adampstringer@protonmail.com>
2023-12-11 12:48:41 +0000

    test: define some test cases

--- src/view_keybinds_test.v
+++ src/view_keybinds_test.v
@@ -6,6 +6,7 @@ import term.ui as tui
 struct KeyEventTestCase {
 	name                string
 	code                tui.KeyCode
+	starting_cursor_pos Pos
 	expected_cursor_pos Pos
 }
 
@@ -13,85 +14,43 @@ const cases = [
 	KeyEventTestCase{
 		name: "key code h"
 		code: tui.KeyCode.h,
-		expected_cursor_pos: Pos{ x: 0, y: 0 }
+		starting_cursor_pos: Pos{ x: 3, y: 0 }
+		expected_cursor_pos: Pos{ x: 2, y: 0 }
 	},
 	KeyEventTestCase{
 		name: "key code l",
 		code: tui.KeyCode.l,
-		expected_cursor_pos: Pos{ x: 1, y: 0 }
+		starting_cursor_pos: Pos{ x: 3, y: 0 }
+		expected_cursor_pos: Pos{ x: 4, y: 0 }
 	},
 	KeyEventTestCase{
 		name: "key code j",
 		code: tui.KeyCode.j,
+		starting_cursor_pos: Pos{ x: 0, y: 0 }
 		expected_cursor_pos: Pos{ x: 0, y: 1 }
+	},
+	KeyEventTestCase{
+		name: "key code k",
+		code: tui.KeyCode.k,
+		starting_cursor_pos: Pos{ x: 0, y: 1 }
+		expected_cursor_pos: Pos{ x: 0, y: 0 }
 	}
 ]
 
 fn test_sets_of_key_events_for_views_on_key_down_adjusting_cursor_position() {
-	mut clip := clipboard.new()
-	mut editor := Editor{ clipboard: mut clip, file_finder_modal: unsafe { nil } }
-	mut fake_view := View{ log: unsafe { nil }, mode: .normal, clipboard: mut clip }
-	fake_view.buffer.lines = [
-		"1. first line",
-		"2. second line",
-		"3. third line"
-	]
-
 	for case in cases {
+		mut clip := clipboard.new()
+		mut editor := Editor{ clipboard: mut clip, file_finder_modal: unsafe { nil } }
+		mut fake_view := View{ log: unsafe { nil }, mode: .normal, clipboard: mut clip }
+		fake_view.buffer.lines = [
+			"1. first line",
+			"2. second line",
+			"3. third line"
+		]
+		fake_view.cursor.pos = case.starting_cursor_pos
 		kevent := &tui.Event{ code: case.code }
 		fake_view.on_key_down(kevent, mut editor)
-		println(case.name)
 		assert fake_view.cursor.pos == case.expected_cursor_pos, 'test case ${case.name} - expected cursor pos assertion failed'
-
-		fake_view.cursor.pos = Pos{}
 	}
 }
 
-fn test_keybind_interp_from_key_code_event_h() {
-	mut clip := clipboard.new()
-	mut editor := Editor{ clipboard: mut clip, file_finder_modal: unsafe { nil } }
-	mut fake_view := View{ log: unsafe { nil }, mode: .normal, clipboard: mut clip }
-	fake_view.buffer.lines = [
-		"1. first line",
-		"2. second line",
-		"3. third line"
-	]
-
-	kevent := &tui.Event{ code: tui.KeyCode.h }
-	fake_view.on_key_down(kevent, mut editor)
-
-	assert fake_view.cursor.pos.x == 0
-}
-
-fn test_keybind_interp_from_key_code_event_l() {
-	mut clip := clipboard.new()
-	mut editor := Editor{ clipboard: mut clip, file_finder_modal: unsafe { nil } }
-	mut fake_view := View{ log: unsafe { nil }, mode: .normal, clipboard: mut clip }
-	fake_view.buffer.lines = [
-		"1. first line",
-		"2. second line",
-		"3. third line"
-	]
-
-	kevent := &tui.Event{ code: tui.KeyCode.l }
-	fake_view.on_key_down(kevent, mut editor)
-
-	assert fake_view.cursor.pos.x == 1
-}
-
-fn test_keybind_interp_from_key_code_event_j() {
-	mut clip := clipboard.new()
-	mut editor := Editor{ clipboard: mut clip, file_finder_modal: unsafe { nil } }
-	mut fake_view := View{ log: unsafe { nil }, mode: .normal, clipboard: mut clip }
-	fake_view.buffer.lines = [
-		"1. first line",
-		"2. second line",
-		"3. third line"
-	]
-
-	kevent := &tui.Event{ code: tui.KeyCode.j }
-	fake_view.on_key_down(kevent, mut editor)
-
-	assert fake_view.cursor.pos.x == 0
-	assert fake_view.cursor.pos.y == 1
-}
