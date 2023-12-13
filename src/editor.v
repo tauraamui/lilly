584962b747a1ac9f8bdc889dfef9b9468d021b30
tauraamui <adampstringer@protonmail.com>
2023-12-13 02:01:34 +0000

    refactor: tidy indentation

--- src/editor.v
+++ src/editor.v
@@ -97,7 +97,7 @@ fn (mut editor Editor) close_file_finder() {
 }
 
 pub fn (mut editor Editor) draw(mut ctx draw.Contextable) {
-  editor.view.draw(mut ctx)
+	editor.view.draw(mut ctx)
 	if editor.file_finder_modal_open {
 		editor.file_finder_modal.draw(mut ctx)
 	}
