467c4efdeedc15fede390651e1520ef747d0d34d
tauraamui <adampstringer@protonmail.com>
2023-12-13 19:28:07 +0000

    fix: properly pass rendering state around

--- src/lib/draw/gui_d_gui.v
+++ src/lib/draw/gui_d_gui.v
@@ -6,31 +6,75 @@ import sokol.gfx
 import sokol.sgl
 import fontstash
 import sokol.sfons
+import os
 
 struct Context{
+	state       &State
 	ref         sapp.Desc
+}
+
+struct State {
+mut:
+	user_data    voidptr
 	frame_cb    fn (v voidptr)
 	pass_action gfx.PassAction
 	fons        &fontstash.Context = unsafe { nil }
+	font_normal int
+	inited      bool
 }
 
+const black = sfons.rgba(0, 0, 0, 255)
+
 pub fn new_context(cfg Config) &Contextable {
-	return Context{
-		frame_cb: cfg.frame_fn
+	mut color_action := gfx.ColorAttachmentAction{
+		load_action: .clear
+		clear_value: gfx.Color{
+			r: 1.0
+			g: 1.0
+			b: 1.0
+			a: 1.0
+		}
+	}
+	mut pass_action := gfx.PassAction{}
+	pass_action.colors[0] = color_action
+	state := &State{
+		pass_action: pass_action
+		user_data: cfg.user_data
+	}
+	return &Context{
+		state: state
 		ref: sapp.Desc{
-			user_data: cfg.user_data
-			init_userdata_cb: fn (v voidptr) {
+			user_data: state
+			init_userdata_cb: fn (mut state State) {
 				desc := sapp.create_desc()
 				gfx.setup(&desc)
 				s := &sgl.Desc{}
 				sgl.setup(s)
+				state.fons = sfons.create(512, 512, 1)
+				if bytes := os.read_bytes(os.resource_abs_path("../experiment/RobotoMono-Regular.ttf")) {
+					println("loaded font: ${bytes.len}")
+					state.font_normal = state.fons.add_font_mem("sans", bytes, false)
+				}
 			}
-			frame_userdata_cb: fn [cfg] (v voidptr) {
-				cfg.frame_fn(v)
-				gfx.begin_default_pass(gfx.PassAction{}, sapp.width(), sapp.height())
+			frame_userdata_cb: fn [cfg] (mut state State) {
+				mut fons := state.fons
+				if !state.inited {
+					fons.clear_state()
+					sgl.defaults()
+					sgl.matrix_mode_projection()
+					sgl.ortho(0.0, f32(sapp.width()), f32(sapp.height()), 0.0, -1.0, 1.0)
+					fons.set_font(state.font_normal)
+					fons.set_color(black)
+					fons.set_size(18)
+					state.inited = true
+				}
+				cfg.frame_fn(state.user_data)
+				/*
+				gfx.begin_default_pass(&state.pass_action, sapp.width(), sapp.height())
 				sgl.draw()
 				gfx.end_pass()
 				gfx.commit()
+				*/
 			}
 			window_title: "lilly".str
 			width: 800
@@ -46,7 +90,9 @@ fn (mut ctx Context) window_height() int { return sapp.height() }
 
 fn (mut ctx Context) set_cursor_position(x int, y int) {}
 
-fn (mut ctx Context) draw_text(x int, y int, text string) {}
+fn (mut ctx Context) draw_text(x int, y int, text string) {
+	ctx.state.fons.draw_text(x, y, text)
+}
 
 fn (mut ctx Context) write(c string) {}
 
@@ -70,6 +116,13 @@ fn (mut ctx Context) run() ! {
 	sapp.run(&ctx.ref)
 }
 
-fn (mut ctx Context) clear() {}
+fn (mut ctx Context) clear() {
+	gfx.begin_default_pass(&ctx.state.pass_action, sapp.width(), sapp.height())
+	sgl.draw()
+	gfx.end_pass()
+	gfx.commit()
+}
 
-fn (mut ctx Context) flush() {}
+fn (mut ctx Context) flush() {
+	sfons.flush(ctx.state.fons)
+}
