9ed5dae821eb77ddf75eac7aedcebaf2ba30d1a2
tauraamui <adampstringer@protonmail.com>
2023-12-20 14:39:07 +0000

    group text draws into multiple passes for sokol

--- src/lib/draw/gui_d_gui.v
+++ src/lib/draw/gui_d_gui.v
@@ -9,10 +9,11 @@ struct Context {
 	user_data voidptr
 	frame_cb fn (v voidptr)
 mut:
-	gg               &gg.Context = unsafe { nil }
-	txt_cfg          gx.TextCfg
-	foreground_color Color
-	background_color Color
+	gg                         &gg.Context = unsafe { nil }
+	txt_cfg                    gx.TextCfg
+	foreground_color           Color
+	background_color           Color
+	text_draws_since_last_pass int
 }
 
 pub fn new_context(cfg Config) &Contextable {
@@ -42,9 +43,11 @@ fn frame(mut ctx Context) {
 	ctx.txt_cfg = gx.TextCfg{
 		size: font_size * int(scale_factor)
 	}
-	ctx.gg.begin()
 	ctx.frame_cb(ctx.user_data)
-	ctx.gg.end(how: .passthru)
+	if ctx.text_draws_since_last_pass < 1000 {
+		ctx.text_draws_since_last_pass = 0
+		ctx.gg.end()
+	}
 }
 
 fn (mut ctx Context) rate_limit_draws() bool { return false }
@@ -56,10 +59,17 @@ fn (mut ctx Context) window_height() int { return gg.window_size().height }
 fn (mut ctx Context) set_cursor_position(x int, y int) {}
 
 fn (mut ctx Context) draw_text(x int, y int, text string) {
-	ctx.gg.begin()
 	// this offsetting stuff is a bit mental but seems to be correct
+	if ctx.text_draws_since_last_pass == 0 {
+		ctx.gg.begin()
+	}
 	ctx.gg.draw_text((font_size / 2) + x - (font_size / 2), (y * font_size) - font_size, text, ctx.txt_cfg)
-	ctx.gg.end(how: .passthru)
+	if ctx.text_draws_since_last_pass >= 1000 {
+		ctx.gg.end(how: .passthru)
+		ctx.text_draws_since_last_pass = 0
+		return
+	}
+	ctx.text_draws_since_last_pass += 1
 }
 
 fn (mut ctx Context) write(c string) {}
