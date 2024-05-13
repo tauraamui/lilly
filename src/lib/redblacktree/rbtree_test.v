618b8291235d07b1527e609b3450ea5328d17634
tauraamui <adampstringer@protonmail.com>
2024-05-13 19:54:46 +0100

    test: remove temp blocking comment block

--- src/lib/redblacktree/rbtree_test.v
+++ src/lib/redblacktree/rbtree_test.v
@@ -23,12 +23,10 @@ fn test_red_black_tree_get() {
 	tree.put(1, "x") // 1->x
 	tree.put(2, "b") // 1->x, 2->b (in order)
 	tree.put(1, "a") // 1->a, 2->b (in order, replacement)
-	/*
 	tree.put(3, "c") // 1->a, 2->b, 3->c (in order)
 	tree.put(4, "d") // 1->a, 2->b, 3->c, 4->d (in order)
 	tree.put(5, "e") // 1->a, 2->b, 3->c, 4->d, 5->e (in order)
 	tree.put(6, "f") // 1->a, 2->b, 3->c, 4->d, 5->e, 6->f (in order)
-	*/
 }
 
 fn test_new_tree_with_some_puts() {
