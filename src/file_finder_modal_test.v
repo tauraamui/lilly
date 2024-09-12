61c21686e0a9d67a5f52169ad3dca16efd5c5c7d
tauraamui <adampstringer@protonmail.com>
2024-09-12 10:18:31 +0100

    test(file finder): ensure selection goes to top on search term adjust

--- src/file_finder_modal_test.v
+++ src/file_finder_modal_test.v
@@ -1,5 +1,26 @@
 module main
 
+import lib.draw
+
+fn test_current_selection_gets_zeros_on_search_term_amend() {
+	mut mock_modal := FileFinderModal{
+		file_paths: [
+			'./src/project/main.v',
+			'./src/project/lib/some_utilities.v',
+		]
+	}
+
+	assert mock_modal.current_selection == 0
+	mock_modal.on_key_down(draw.Event{utf8: "d"}, mut Editor{})
+	assert mock_modal.current_selection == 0
+
+	mock_modal.on_key_down(draw.Event{code: .down}, mut Editor{})
+	assert mock_modal.current_selection == 1
+
+	mock_modal.on_key_down(draw.Event{utf8: "r"}, mut Editor{})
+	assert mock_modal.current_selection == 0
+}
+
 fn test_resolve_file_paths_returns_realistic_results() {
 	mut mock_modal := FileFinderModal{
 		file_paths: [
