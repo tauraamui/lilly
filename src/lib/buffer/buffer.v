b98109f2f797c4c12cc345f098c79c562c720759
tauraamui <adampstringer@protonmail.com>
2024-12-06 22:40:00 +0000

    chore: remove unused methods and invocations

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -24,30 +24,3 @@ pub fn (mut buffer Buffer) load_from_path() ! {
 	}
 }
 
-pub fn (mut buffer Buffer) undo() {
-	op_to_undo := buffer.history.pop_undo() or { return }
-	mut line_offset := 0
-	match op_to_undo.kind {
-		'ins' {
-			buffer.lines.delete(op_to_undo.line_num + line_offset)
-			line_offset -= 1
-		}
-		'del' {
-			buffer.lines.insert(op_to_undo.line_num + line_offset, op_to_undo.value)
-			line_offset += 1
-		}
-		else {}
-	}
-}
-
-pub fn (mut buffer Buffer) snapshot() {
-	buffer.snapshotted_at_least_once = true
-	buffer.lines_cpy = buffer.lines.clone()
-}
-
-pub fn (mut buffer Buffer) update_undo_history() {
-	if !buffer.snapshotted_at_least_once {
-		return
-	}
-	buffer.history.append_ops_to_undo(buffer.lines_cpy, buffer.lines)
-}
