e4bccd33f263db2d58bcce4e3fee47d5de69ea74
tauraamui <adampstringer@protonmail.com>
2024-12-08 14:30:13 +0000

    feat: load gap buffer data if feat toggled

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -13,18 +13,18 @@ mut:
 	// line_tracker LineTracker
 }
 
-pub fn (mut buffer Buffer) load_from_path() ! {
+pub fn (mut buffer Buffer) load_from_path(use_gap_buffer bool) ! {
 	buffer.lines = os.read_lines(buffer.file_path) or {
 		return error('unable to open file ${buffer.file_path} ${err}')
 	}
 	if buffer.lines.len == 0 {
 		buffer.lines = ['']
 	}
-	// TODO(tauraamui): enable this additional loading under a flag
-	/*
-	file_contents := os.read_file(buffer.file_path) or { return error("unable to open file ${buffer.file_path}: ${err}") }
-	buffer.c_buffer = GapBuffer.new(file_contents)
-	*/
+
+	if use_gap_buffer {
+		file_contents := os.read_file(buffer.file_path) or { return error("unable to open file ${buffer.file_path}: ${err}") }
+		buffer.c_buffer = GapBuffer.new(file_contents)
+	}
 }
 
 pub fn (mut buffer Buffer) iterator() LineIterator {
