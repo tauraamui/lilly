973fbe93d69c930651fec396e16e0b521a092b38
tauraamui <adampstringer@protonmail.com>
2024-12-08 14:55:49 +0000

    feat: return generic iterator type
    If the user has passed in the "use gap buffer" toggle flag, then
    the iterator method on the buffer type will return a special gap
    buffer specific iterator. If not, the iterator method will return
    a pretty basic iterator wrapper for the document lines list.

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -9,7 +9,8 @@ pub mut:
 	lines            []string
 	auto_close_chars []string
 mut:
-	c_buffer   GapBuffer
+	use_gap_buffer   bool
+	c_buffer         GapBuffer
 	// line_tracker LineTracker
 }
 
@@ -22,17 +23,40 @@ pub fn (mut buffer Buffer) load_from_path(use_gap_buffer bool) ! {
 	}
 
 	if use_gap_buffer {
+		buffer.use_gap_buffer = use_gap_buffer
 		file_contents := os.read_file(buffer.file_path) or { return error("unable to open file ${buffer.file_path}: ${err}") }
 		buffer.c_buffer = GapBuffer.new(file_contents)
 	}
 }
 
-pub fn (mut buffer Buffer) iterator() LineIterator {
-	return new_iterator(buffer.c_buffer)
+pub interface LineIterator {
+mut:
+	next() ?string
 }
 
-fn new_iterator(buffer GapBuffer) LineIterator {
-	return LineIterator{
+struct LineListIterator {
+mut:
+	index    int
+	data_ref []string
+}
+
+pub fn (mut iter LineListIterator) next() ?string {
+	if iter.index >= iter.data_ref.len {
+		return none
+	}
+	defer { iter.index += 1 }
+	return iter.data_ref[iter.index]
+}
+
+pub fn (mut buffer Buffer) iterator() LineIterator {
+	if buffer.use_gap_buffer {
+		return new_iterator(buffer.c_buffer)
+	}
+	return LineListIterator{ data_ref: buffer.lines }
+}
+
+fn new_iterator(buffer GapBuffer) GapBufferLineIterator {
+	return GapBufferLineIterator{
 		data: buffer.str()
 	}
 }
