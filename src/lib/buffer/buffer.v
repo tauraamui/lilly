a0306e1016b911b59f27041937f4ee0e3f15f59b
Adam Stringer <adampstringer@protonmail.com>
2024-12-08 15:01:22 +0000

    Feat/add feat flag for migrating to gap buffer (#252)
    * chore: remove unused imports
    
    * feat: add flag to enable using gap buffer
    
    * feat: pass gap buffer feat toggle into editor constructor
    
    * feat: store gap buffer toggle into editor instance
    
    * feat: load gap buffer data if feat toggled
    
    * feat: return generic iterator type
    
    If the user has passed in the "use gap buffer" toggle flag, then
    the iterator method on the buffer type will return a special gap
    buffer specific iterator. If not, the iterator method will return
    a pretty basic iterator wrapper for the document lines list.
    
    * chore: rename gap buffers iterator type

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -1,6 +1,5 @@
 module buffer
 
-import history { History }
 import os
 
 pub struct Buffer {
@@ -10,30 +9,54 @@ pub mut:
 	lines            []string
 	auto_close_chars []string
 mut:
-	c_buffer   GapBuffer
+	use_gap_buffer   bool
+	c_buffer         GapBuffer
 	// line_tracker LineTracker
 }
 
-pub fn (mut buffer Buffer) load_from_path() ! {
+pub fn (mut buffer Buffer) load_from_path(use_gap_buffer bool) ! {
 	buffer.lines = os.read_lines(buffer.file_path) or {
 		return error('unable to open file ${buffer.file_path} ${err}')
 	}
 	if buffer.lines.len == 0 {
 		buffer.lines = ['']
 	}
-	// TODO(tauraamui): enable this additional loading under a flag
-	/*
-	file_contents := os.read_file(buffer.file_path) or { return error("unable to open file ${buffer.file_path}: ${err}") }
-	buffer.c_buffer = GapBuffer.new(file_contents)
-	*/
+
+	if use_gap_buffer {
+		buffer.use_gap_buffer = use_gap_buffer
+		file_contents := os.read_file(buffer.file_path) or { return error("unable to open file ${buffer.file_path}: ${err}") }
+		buffer.c_buffer = GapBuffer.new(file_contents)
+	}
+}
+
+pub interface LineIterator {
+mut:
+	next() ?string
+}
+
+struct LineListIterator {
+mut:
+	index    int
+	data_ref []string
+}
+
+pub fn (mut iter LineListIterator) next() ?string {
+	if iter.index >= iter.data_ref.len {
+		return none
+	}
+	defer { iter.index += 1 }
+	return iter.data_ref[iter.index]
 }
 
 pub fn (mut buffer Buffer) iterator() LineIterator {
-	return new_iterator(buffer.c_buffer)
+	if buffer.use_gap_buffer {
+		return new_iterator(buffer.c_buffer)
+	}
+	return LineListIterator{ data_ref: buffer.lines }
 }
 
-fn new_iterator(buffer GapBuffer) LineIterator {
-	return LineIterator{
+fn new_iterator(buffer GapBuffer) GapBufferLineIterator {
+	return GapBufferLineIterator{
 		data: buffer.str()
 	}
 }
