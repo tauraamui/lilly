b60a5b31adab0bf9e839a98b848df4426eee9710
tauraamui <adampstringer@protonmail.com>
2024-12-11 00:36:16 +0000

    chore: define insert method on buffer wrapper type

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -6,10 +6,17 @@ pub:
 pub mut:
 	lines            []string
 	auto_close_chars []string
+	cursor           Pos
 mut:
 	use_gap_buffer   bool
 	c_buffer         GapBuffer
 	// line_tracker LineTracker
+}
+
+pub struct Pos {
+pub mut:
+	x int
+	y int
 }
 
 pub fn (mut buffer Buffer) load_from_path(read_lines fn (path string) ![]string, use_gap_buffer bool) ! {
@@ -25,6 +32,45 @@ pub fn (mut buffer Buffer) load_from_path(read_lines fn (path string) ![]string,
 		file_contents := buffer.lines.join("\n")
 		buffer.c_buffer = GapBuffer.new(file_contents)
 	}
+}
+
+pub fn (mut buffer Buffer) move_cursor_to(x int, y int) {
+	buffer.cursor.x = x
+	buffer.cursor.y = y
+	if buffer.use_gap_buffer {
+		// TODO(tauraamui): move the gap to the correct position offset
+	}
+}
+
+pub fn (mut buffer Buffer) insert_text(s string) {
+	if buffer.use_gap_buffer {
+		buffer.c_buffer.insert(s)
+		return
+	}
+
+	line := buffer.lines[buffer.cursor.y]
+
+	defer { buffer.cursor.x += s.runes().len }
+	if line.len == 0 {
+		buffer.lines[buffer.cursor.y] = "${s}"
+		buffer.cursor.x = s.runes().len
+		return
+	} else {
+		if buffer.cursor.x > line.len {
+			buffer.cursor.x = line.len
+		}
+		uline := line.runes()
+		if buffer.cursor.x > uline.len {
+			return
+		}
+
+		left := uline[..buffer.cursor.x].string()
+		right := uline[buffer.cursor.x..uline.len].string()
+
+		buffer.lines[buffer.cursor.y] = "${left}${s}${right}"
+	}
+
+	buffer.cursor.x += s.runes().len
 }
 
 pub interface Iterator {
