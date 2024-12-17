2862521b5bbc05aeff7d57399ae38f16ac5d539e
tauraamui <adampstringer@protonmail.com>
2024-12-17 16:22:18 +0000

    test: change desired state comparison to reflect desired behaviour

--- src/view_gap_buffer_test.v
+++ src/view_gap_buffer_test.v
@@ -25,7 +25,7 @@ fn test_insert_text() {
 	]
 }
 
-fn test_o_inserts_empty_line() {
+fn test_shift_o_inserts_empty_line_above_current() {
 	mut clip := clipboardv2.new()
 	mut fake_view := View{
 		log: log.Log{}
@@ -35,15 +35,46 @@ fn test_o_inserts_empty_line() {
 
 	fake_view.buffer.use_gap_buffer = true
 	// manually set the "document" contents
-	fake_view.buffer.load_contents_into_gap("1. first line\n2. second line")
+	fake_view.buffer.load_contents_into_gap("1. first line\n2. second line\n3. third line")
+
+	fake_view.cursor.pos.y = 1
+
+	fake_view.shift_o()
+
+	assert fake_view.leader_state.mode == .insert
+	lines := fake_view.buffer.str().split("\n")
+	assert lines == [
+		"1. first line"
+		""
+		"2. second line"
+		"3. third line"
+	]
+	assert fake_view.cursor.pos.y == 1
+}
+
+fn test_o_inserts_empty_line_below_current() {
+	mut clip := clipboardv2.new()
+	mut fake_view := View{
+		log: log.Log{}
+		leader_state: ViewLeaderState{ mode: .normal }
+		clipboard: mut clip
+	}
+
+	fake_view.buffer.use_gap_buffer = true
+	// manually set the "document" contents
+	fake_view.buffer.load_contents_into_gap("1. first line\n2. second line\n3. third line")
+
+	fake_view.cursor.pos.y = 1
 
 	fake_view.o()
 
+	assert fake_view.leader_state.mode == .insert
 	lines := fake_view.buffer.str().split("\n")
 	assert lines == [
-		"1. first line",
-		""
+		"1. first line"
 		"2. second line"
+		""
+		"3. third line"
 	]
-	assert fake_view.cursor.pos.y == 1
+	assert fake_view.cursor.pos.y == 2
 }
