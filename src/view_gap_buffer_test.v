5514c6faec1ec0d2ea3bdcf3b3e17478df6a32e3
tauraamui <adampstringer@protonmail.com>
2024-12-21 02:14:39 +0000

    test; ensure we highlight the final case that needs redoing

--- src/view_gap_buffer_test.v
+++ src/view_gap_buffer_test.v
@@ -228,6 +228,31 @@ fn test_w_moves_to_start_of_next_word() {
 	assert fake_view.cursor.pos.y == 0
 }
 
+fn test_w_moves_to_start_of_next_line_if_on_empty_line() {
+	mut clip := clipboardv2.new()
+	mut fake_view := View{
+		log: log.Log{}
+		leader_state: ViewLeaderState{ mode: .normal }
+		clipboard: mut clip
+	}
+
+	fake_view.buffer.use_gap_buffer = true
+	// manually set the "document" contents
+	fake_view.buffer.load_contents_into_gap("This is the first line.\n\n2. second line")
+
+	fake_view.cursor.pos.x = 0
+	fake_view.cursor.pos.y = 1
+
+	fake_view.w()
+	assert fake_view.cursor.pos.x == 0
+	assert fake_view.cursor.pos.y == 2
+
+	fake_view.w()
+	assert fake_view.cursor.pos.x == 8
+	assert fake_view.cursor.pos.y == 0
+}
+
+
 fn test_w_moves_to_start_of_next_word_across_a_newline() {
 	mut clip := clipboardv2.new()
 	mut fake_view := View{
@@ -271,6 +296,6 @@ fn test_w_moves_to_start_of_next_word_up_to_document_end() {
 	fake_view.cursor.pos.y = 1
 
 	fake_view.w()
-	// assert fake_view.cursor.pos.x == 10
-	// assert fake_view.cursor.pos.y == 1
+	assert fake_view.cursor.pos.x == 10
+	assert fake_view.cursor.pos.y == 1
 }
