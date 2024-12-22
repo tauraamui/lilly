295f0c8f96a34f2b40425784136c9456650f43ad
tauraamui <adampstringer@protonmail.com>
2024-12-22 17:00:13 +0000

    test: define next e motion case to describe desired behaviour

--- src/view_gap_buffer_test.v
+++ src/view_gap_buffer_test.v
@@ -403,6 +403,27 @@ fn test_w_moves_to_start_of_next_word_from_whitespace() {
 	assert fake_view.cursor.pos.y == 0
 }
 
+fn test_w_moves_to_end_of_next_word() {
+	mut clip := clipboardv2.new()
+	mut fake_view := View{
+		log: log.Log{}
+		leader_state: ViewLeaderState{ mode: .normal }
+		clipboard: mut clip
+	}
+
+	fake_view.buffer.use_gap_buffer = true
+	// manually set the "document" contents
+	fake_view.buffer.load_contents_into_gap("This is the first line.\n2. second line")
+
+	fake_view.e()
+	assert fake_view.cursor.pos.x == 3
+	assert fake_view.cursor.pos.y == 0
+
+	fake_view.e()
+	assert fake_view.cursor.pos.x == 3
+	assert fake_view.cursor.pos.y == 0
+}
+
 fn test_e_moves_from_blank_line_to_next() {
 	mut clip := clipboardv2.new()
 	mut fake_view := View{
