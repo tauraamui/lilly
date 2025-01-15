3200f3d9e100ccdb835a5a2dd4679d6b4fec39aa
Adam Stringer <adampstringer@protonmail.com>
2025-01-15 00:25:14 +0000

    test: ensure insert tab behaves as expected for gap buffer backing structure (#296)

--- src/view_gap_buffer_test.v
+++ src/view_gap_buffer_test.v
@@ -31,6 +31,31 @@ fn test_insert_text() {
 	]
 }
 
+fn test_insert_tab() {
+	mut clip := clipboardv2.new()
+	mut fake_view := View{
+		log: log.Log{}
+		leader_state: ViewLeaderState{ mode: .normal }
+		clipboard: mut clip
+	}
+
+	fake_view.buffer.use_gap_buffer = true
+	// manually set the "document" contents
+	fake_view.buffer.load_contents_into_gap("1. first line\n2. second line")
+
+	fake_view.cursor.pos.x = 3
+	fake_view.insert_tab()
+
+	lines := fake_view.buffer.str().split("\n")
+	assert lines == [
+		"1.     first line",
+		"2. second line"
+	]
+
+	assert fake_view.cursor.pos.x == 7
+	assert fake_view.cursor.pos.y == 0
+}
+
 fn test_shift_o_inserts_empty_line_above_current() {
 	mut clip := clipboardv2.new()
 	mut fake_view := View{
