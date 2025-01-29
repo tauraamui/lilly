cfbbdc08433200311acb16146ba2f8f866f68ad3
tauraamui <adampstringer@protonmail.com>
2025-01-29 14:14:15 +0000

    fix: correct lps and kmp functions

--- experiment/pattern_search/search.v
+++ experiment/pattern_search/search.v
@@ -1,60 +1,55 @@
 module main
 
-fn compute_lps_buffer(pattern string, mut lps []int) {
-	mut length := 0
-
-	// lps index 0 is always 0
-	lps[0] = 0
-
+fn compute_lps(pattern string, mut lps []int) {
 	mut i := 1
+	mut j := 0
+
 	for i < pattern.len {
-		if pattern[i] == pattern[length] {
-			length += 1
-			lps[i] = length
+		if pattern[i] == pattern[j] {
+			j += 1
+			lps[i] = j
 			i += 1
 			continue
 		}
-		if length != 0 {
-			length = lps[length - 1]
+		if j > 0 {
+			j = lps[j - 1]
 			continue
 		}
 		lps[i] = 0
 		i += 1
 	}
-	println(lps)
 }
 
-fn kmps_earch(text string, pattern string) {
-	text_length := text.len
-	pattern_length := pattern.len
-	mut lps := []int{ len: pattern_length }
-	// Preallocated memory for LPS array
-	compute_lps_buffer(pattern, mut lps)
+fn kmp(text string, pattern string) int {
+	mut lps := []int{ len: pattern.len }
+	compute_lps(pattern, mut lps)
+
 	mut i := 0
-	// index for text
 	mut j := 0
-	// index for pattern
-	for i < text_length {
-		if pattern[j] == text[i] {
+
+	for i < text.len {
+		if text[i] == pattern[j] {
+			if j == pattern.len - 1 {
+				return i - j
+			}
 			i += 1
 			j += 1
+			continue
 		}
-		if j == pattern_length {
-			println("Pattern found at index ${i - j}")
+		// use lps to skip comps
+		if j > 0 {
 			j = lps[j - 1]
-		} else if i < text_length && pattern[j] != text[i] {
-			if j != 0 {
-				j = lps[j - 1]
-			} else {
-				i += 1
-			}
+		} else {
+			i += 1
 		}
 	}
+
+	return -1
 }
 
 fn main() {
 	text := 'ABABDABACDABABCABAB'
 	pattern := 'ABABCABAB'
-	kmps_earch(text, pattern)
+	kmp(text, pattern)
 	return
 }
