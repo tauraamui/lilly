c41128f66c25055afd5e615666794f37f627532b
tauraamui <adampstringer@protonmail.com>
2025-01-29 14:14:26 +0000

    test: ensure pattern finding works correctly

--- experiment/pattern_search/search_test.v
+++ experiment/pattern_search/search_test.v
@@ -3,7 +3,17 @@ module main
 fn test_compute_lps_buffer_from_pattern() {
 	pattern := "ABACDABAB"
 	mut lsp := []int{ len: pattern.len }
-	compute_lps_buffer(pattern, mut &lsp)
+	compute_lps(pattern, mut lsp)
 	assert lsp == [0, 0, 1, 0, 0, 1, 2, 3, 2]
+}
+
+fn test_kmp_search() {
+	mut text := "// TODO(tauraamui) [29/01/25]: some comment contents"
+	mut pattern := "TODO"
+	assert kmp(text, pattern) == 3
+
+	text = "ABABDABACDABABCABAB"
+	pattern = "ABACDABAB"
+	assert kmp(text, pattern) == 5
 }
 
