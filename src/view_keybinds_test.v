92d8815a9064a442458ea9c62fae94902f169094
tauraamui <adampstringer@protonmail.com>
2025-02-07 16:57:17 +0000

    fix: change back accidental rename of editor in license header

--- src/view_keybinds_test.v
+++ src/view_keybinds_test.v
@@ -1,4 +1,4 @@
-// Copyright 2024 The Lilly lilly contributors
+// Copyright 2024 The Lilly Editor contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
