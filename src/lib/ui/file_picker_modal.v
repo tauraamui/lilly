84018c8a5fb7ed30168485e4a7d6f0268611475e
tauraamui <adampstringer@protonmail.com>
2025-02-14 00:52:57 +0000

    fix: assign file paths data via constructor and add closed check helper

--- src/lib/ui/file_picker_modal.v
+++ src/lib/ui/file_picker_modal.v
@@ -2,17 +2,22 @@ module ui
 
 @[noinit]
 pub struct FilePickerModal {
+	file_paths []string
 mut:
 	open bool
 }
 
 pub fn FilePickerModal.new(file_paths []string) FilePickerModal {
-	return FilePickerModal{}
+	return FilePickerModal{
+		file_paths: file_paths
+	}
 }
 
 pub fn (mut f_picker FilePickerModal) open() {
 	f_picker.open = true
 }
+
+pub fn (f_picker FilePickerModal) is_open() bool { return f_picker.open }
 
 pub fn (mut f_picker FilePickerModal) close() {
 	f_picker.open = false
