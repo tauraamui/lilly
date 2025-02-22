d3733391ada583d4569392490dab3024c4189b6b
tauraamui <adampstringer@protonmail.com>
2025-02-22 02:34:07 +0000

    test: ensure that open_file_v2 only results in once buffer and buffer view

--- src/lilly_test.v
+++ src/lilly_test.v
@@ -87,17 +87,56 @@ fn test_lilly_open_file_v2_loads_into_file_buffer_and_buffer_view_maps() {
 
 	assert lilly.views.len == 0
 	assert lilly.buffers.len == 0
+	assert lilly.file_buffers.len == 0
+	assert lilly.buffer_views.len == 0
 
 	lilly.open_file_with_reader_v2("test-file.txt", m_line_reader.read_lines) or { assert false }
 
 	assert m_line_reader.given_path == "test-file.txt"
-	assert lilly.views.len   == 0
-	assert lilly.buffers.len == 0
+	assert lilly.file_buffers.len == 1
+	assert lilly.buffer_views.len == 1
 
 	file_buff := lilly.file_buffers["test-file.txt"] or { assert false, "failed to find buffer instance for path: test-file.txt" }
 	buff_view := lilly.buffer_views[file_buff.uuid]  or { assert false, "failed to find view instance for buffer of uuid: ${file_buff.uuid}" }
 	assert lilly.view == buff_view
 }
+
+fn test_lilly_open_file_v2_loads_into_file_buffer_and_buffer_view_maps_if_done_twice_does_not_create_extra_instances() {
+	mut clip := clipboardv2.new()
+	mut lilly := Lilly{
+		clipboard: mut clip
+	}
+
+	mut m_line_reader := MockLineReader{
+		line_data: ["This is a fake document that doesn't exist on disk anywhere"]
+	}
+
+	assert lilly.views.len == 0
+	assert lilly.buffers.len == 0
+	assert lilly.file_buffers.len == 0
+	assert lilly.buffer_views.len == 0
+
+	lilly.open_file_with_reader_v2("test-file.txt", m_line_reader.read_lines) or { assert false }
+
+	assert m_line_reader.given_path == "test-file.txt"
+	assert lilly.file_buffers.len == 1
+	assert lilly.buffer_views.len == 1
+	assert lilly.view.file_path == "test-file.txt"
+
+	mut file_buff := lilly.file_buffers["test-file.txt"] or { assert false, "failed to find buffer instance for path: test-file.txt" }
+	mut buff_view := lilly.buffer_views[file_buff.uuid]  or { assert false, "failed to find view instance for buffer of uuid: ${file_buff.uuid}" }
+	assert lilly.view == buff_view
+
+	lilly.open_file_with_reader_v2("test-file.txt", m_line_reader.read_lines) or { assert false }
+	assert lilly.file_buffers.len == 1
+	assert lilly.buffer_views.len == 1
+	assert lilly.view.file_path == "test-file.txt"
+
+	file_buff = lilly.file_buffers["test-file.txt"] or { assert false, "failed to find buffer instance for path: test-file.txt" }
+	buff_view = lilly.buffer_views[file_buff.uuid]  or { assert false, "failed to find view instance for buffer of uuid: ${file_buff.uuid}" }
+	assert lilly.view == buff_view
+}
+
 
 // TODO(tauraamui) [12/02/2025] something is horrendously broken with the below tests, its so bad that its making the
 //                              v test suite runner have some kind of stroke for all of the other asserts in this file...
