35d295e88c541f2ae30fb50165f4a4e6afe2ba61
tauraamui <adampstringer@protonmail.com>
2025-03-15 21:51:21 +0000

    feat: pass line number to start rendering document from

--- src/lib/ui/buffer_view.v
+++ src/lib/ui/buffer_view.v
@@ -11,9 +11,20 @@ pub fn BufferView.new(buf &buffer.Buffer) BufferView {
 	return BufferView{ buf: buf }
 }
 
-pub fn (buf_view BufferView) draw(mut ctx draw.Contextable, x int, y int, width int, height int) {
-	// if buf_view.buf == unsafe { nil } { return }
-	ctx.set_bg_color(r: 100, g: 100, b: 225)
-	ctx.draw_rect(x + 1, y + 1, width, height)
-	ctx.reset_bg_color()
+pub fn (buf_view BufferView) draw(
+	mut ctx draw.Contextable,
+	x int, y int,
+	width int, height int,
+	from_line_num int
+) {
+	if buf_view.buf == unsafe { nil } { return }
+
+	mut screenspace_y_offset := 1
+	for document_line_num, line in buf_view.buf.line_iterator() {
+		if document_line_num < from_line_num { continue }
+		ctx.draw_text(x + 1, y + screenspace_y_offset, line)
+		screenspace_y_offset += 1
+		if screenspace_y_offset > height { return }
+	}
 }
+
