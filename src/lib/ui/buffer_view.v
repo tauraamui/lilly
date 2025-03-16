5bf4da4a067bfa9fed598f702dda844d412cf41d
tauraamui <adampstringer@protonmail.com>
2025-03-16 13:12:02 +0000

    feat: start to permit buffer view to optionally start and end each line

--- src/lib/ui/buffer_view.v
+++ src/lib/ui/buffer_view.v
@@ -21,10 +21,19 @@ pub fn (buf_view BufferView) draw(
 
 	mut screenspace_y_offset := 1
 	for document_line_num, line in buf_view.buf.line_iterator() {
+		// if we haven't reached the line to render in the document yet, skip this
 		if document_line_num < from_line_num { continue }
-		ctx.draw_text(x + 1, y + screenspace_y_offset, line)
+
+		// draw the line of text, offset by the position of the buffer view
+		draw_text_line(mut ctx, x + 1, y + screenspace_y_offset, line, 0, width)
+
 		screenspace_y_offset += 1
+		// detect if number of lines drawn would exceed current height of view
 		if screenspace_y_offset > height { return }
 	}
+}
+
+fn draw_text_line(mut ctx draw.Contextable, x int, y int, line string, min_x int, width int) {
+	ctx.draw_text(x, y, line)
 }
 
