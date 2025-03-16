a1de540858cc0bcda045f6a3a02fc1d05abaae9b
tauraamui <adampstringer@protonmail.com>
2025-03-16 23:09:27 +0000

    feat: within new buffer view draw line numbers per line

--- src/lib/ui/buffer_view.v
+++ src/lib/ui/buffer_view.v
@@ -21,18 +21,32 @@ pub fn (buf_view BufferView) draw(
 ) {
 	if buf_view.buf == unsafe { nil } { return }
 
+	mut screenspace_x_offset := 1 + buf_view.buf.num_of_lines().str().runes().len
 	mut screenspace_y_offset := 1
 	for document_line_num, line in buf_view.buf.line_iterator() {
 		// if we haven't reached the line to render in the document yet, skip this
 		if document_line_num < from_line_num { continue }
 
+		// draw line number
+		draw_line_number(mut ctx, screenspace_x_offset, y + screenspace_y_offset, document_line_num)
+
 		// draw the line of text, offset by the position of the buffer view
-		draw_text_line(mut ctx, x + 1, y + screenspace_y_offset, line, buf_view.min_x, width)
+		draw_text_line(mut ctx, screenspace_x_offset + 1, y + screenspace_y_offset, line, buf_view.min_x, width)
 
 		screenspace_y_offset += 1
 		// detect if number of lines drawn would exceed current height of view
 		if screenspace_y_offset > height { return }
 	}
+}
+
+const line_num_fg_color = draw.Color{ r: 117, g: 118, b: 120 }
+
+fn draw_line_number(mut ctx draw.Contextable, x int, y int, line_num int) {
+	defer { ctx.reset_color() }
+	ctx.set_color(line_num_fg_color)
+
+	mut line_num_str := "${line_num + 1}"
+	ctx.draw_text(x - line_num_str.runes().len, y, line_num_str)
 }
 
 fn draw_text_line(mut ctx draw.Contextable, x int, y int, line string, min_x int, width int) {
