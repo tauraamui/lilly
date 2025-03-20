1a815ff4ebc5b60b5b4c4ec8ae9f252f98a6466f
tauraamui <adampstringer@protonmail.com>
2025-03-20 21:36:54 +0000

    fix: adjust file license header copyright year to reflect reality

--- src/lib/chords/chords.v
+++ src/lib/chords/chords.v
@@ -1,4 +1,4 @@
-// Copyright 2025 The Lilly Editor contributors
+// Copyright 2024 The Lilly Editor contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
