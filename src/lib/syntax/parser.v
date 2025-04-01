85263a874f9173b5320d65e84b4d5cf372bc7694
Adam Stringer <adampstringer@protonmail.com>
2025-04-01 02:53:03 +0100

    Feat/implement syntax parsing (#349)
    * feat: define parsing functionality
    
    * test: make sure parsing works correctly
    
    * test: define expected remaining comment contents and comment end
    
    * chore: re-write parsing line logic
    
    * test: make manually debugging parsing token logic easier
    
    * wip: massively simplify impl to cover most basic required case
    
    * test: change test code data
    
    * chore: implement char type transition tracking
    
    * fix: clone pending token content data to prevent retrospective mutation
    
    * fix: build tokens which describe data bounds instead of copying
    
    * chore: remove unwanted old impl
    
    * test: temp manual comparison of data from parser
    
    * reflect using just data indexes in test/example usage of scan results
    
    * test: add more asserts and a note
    
    * test: make line 1 tokens more consice
    
    * test/refactor: move line specific value cmps to own func
    
    * test: ensure that line 2s parsed tokens are correct

---
+++ src/lib/syntax/parser.v
@@ -0,0 +1,114 @@
+module syntax
+
+// NOTE(tauraamui) [27/03/2025]: this is ... idk I just feel like trying to write something
+//                               that feels comfier than trying to embed TS's parser.c and
+//                               have a custom scanner thing
+
+enum State {
+	default
+	in_block_comment
+}
+
+enum TokenType {
+	keyword
+	identifier
+	operator
+	string
+	comment
+	comment_start
+	comment_end
+	block_start
+	block_end
+	number
+	whitespace
+	other
+}
+
+pub struct Token {
+	t_type TokenType
+mut:
+	start  int
+	end    int
+}
+
+struct LineInfo {
+	start_token_index int
+	token_count       int
+}
+
+pub struct Parser {
+mut:
+	state         State
+	pending_token ?Token
+	tokens        []Token
+	line_info     []LineInfo
+}
+
+fn (parser Parser) get_line_tokens(line_num int) []Token {
+	if line_num < 0 || line_num >= parser.line_info.len {
+		return []Token{}
+	}
+	line_info   := parser.line_info[line_num]
+	start_index := line_info.start_token_index
+	end_index   := start_index + line_info.token_count
+	return parser.tokens[start_index..end_index]
+}
+
+pub fn (mut parser Parser) parse_lines(lines []string) {
+	for i, line in lines { parser.parse_line(i, line) }
+}
+
+pub fn (mut parser Parser) parse_line(index int, line string) []Token {
+	mut start_token_index := parser.tokens.len
+	mut token_count       := 0
+	mut rune_count        := 0
+	runes                 := line.runes()
+
+	mut i := 0
+	mut token_type := TokenType.other
+
+	mut current_char_type := TokenType.other
+
+	for i < runes.len {
+		mut last_char_type := current_char_type
+		c_char := runes[i]
+		current_char_type = match c_char {
+			` `, `\t` { .whitespace }
+			else { .other }
+		}
+		if i == 0 { last_char_type = current_char_type }
+
+		transition_occurred := last_char_type != current_char_type
+		if transition_occurred {
+			token := Token{
+				t_type: last_char_type
+				start: i - rune_count
+				end: i
+			}
+			parser.tokens << token
+			token_count += 1
+			rune_count = 0
+		}
+
+		rune_count += 1
+		i += 1
+	}
+
+	if rune_count > 0 {
+		token := Token{
+			t_type: .other
+			start: runes.len - rune_count
+			end: runes.len
+		}
+		parser.tokens << token
+		token_count += 1
+	}
+
+	line_info := LineInfo{
+		start_token_index: start_token_index
+		token_count: token_count
+	}
+	parser.line_info << line_info
+	return parser.get_line_tokens(index)
+}
+
---
+++ src/lib/syntax/parser_test.v
@@ -0,0 +1,127 @@
+module syntax
+
+/*
+fn test_parser_manual() {
+	code := "
+// This is a comment
+fn main() {
+	/*
+	 * Block comment
+	 */
+	random_x_int := 10
+	return random_x_int
+}
+"
+
+	mut parser := Parser{}
+
+	code_lines := code.split("\n")
+
+	for i, code_line in code_lines {
+		code_line_chars := code_line.runes()
+		parser.parse_line(code_line)
+		line_tokens := parser.get_line_tokens(i)
+		for j, token in line_tokens {
+			print("${code_line_chars[token.start..token.end].string()}")
+			if j + 1 == line_tokens.len {
+				println("")
+			}
+		}
+		// println("LINE ${i} tokens ${parser.get_line_tokens(i)}")
+		// println(parser.tokens)
+	}
+
+	assert 1 == 3
+}
+*/
+
+fn test_parser_block_of_code_one() {
+	code := "
+// This is a comment
+fn main() {
+	/*
+	 * Block comment
+	 */
+	random_x_int := 10
+	return random_x_int
+}
+"
+
+	mut parser := Parser{}
+	lines := code.split("\n")
+	for i, line in lines {
+		parser.parse_line(i, line)
+	}
+
+	assert parser.get_line_tokens(0) == []
+	assert_line_1_tokens(lines[1], parser.get_line_tokens(1))
+	assert_line_2_tokens(lines[2], parser.get_line_tokens(2))
+}
+
+fn assert_line_1_tokens(line_1 string, line_1_tokens []Token) {
+	assert line_1_tokens.len == 9
+	line_1_token_0 := line_1_tokens[0]
+	line_1_token_1 := line_1_tokens[1]
+	line_1_token_2 := line_1_tokens[2]
+	line_1_token_3 := line_1_tokens[3]
+	line_1_token_4 := line_1_tokens[4]
+	line_1_token_5 := line_1_tokens[5]
+	line_1_token_6 := line_1_tokens[6]
+	line_1_token_7 := line_1_tokens[7]
+	line_1_token_8 := line_1_tokens[8]
+
+	// NOTE(tauraamui) [31/03/2025]: for some reason we cannot assert directly against a
+	//                               string slice like this without v converting it into a string
+	//                               literal
+	assert "${line_1[line_1_token_0.start..line_1_token_0.end]}" == "//"
+	assert line_1_token_0.t_type == .other
+
+	assert "${line_1[line_1_token_1.start..line_1_token_1.end]}" == " "
+	assert line_1_token_1.t_type == .whitespace
+
+	assert "${line_1[line_1_token_2.start..line_1_token_2.end]}" == "This"
+	assert line_1_token_2.t_type == .other
+
+	assert "${line_1[line_1_token_3.start..line_1_token_3.end]}" == " "
+	assert line_1_token_3.t_type == .whitespace
+
+	assert "${line_1[line_1_token_4.start..line_1_token_4.end]}" == "is"
+	assert line_1_token_4.t_type == .other
+
+	assert "${line_1[line_1_token_5.start..line_1_token_5.end]}" == " "
+	assert line_1_token_5.t_type == .whitespace
+
+	assert "${line_1[line_1_token_6.start..line_1_token_6.end]}" == "a"
+	assert line_1_token_6.t_type == .other
+
+	assert "${line_1[line_1_token_7.start..line_1_token_7.end]}" == " "
+	assert line_1_token_7.t_type == .whitespace
+
+	assert "${line_1[line_1_token_8.start..line_1_token_8.end]}" == "comment"
+	assert line_1_token_8.t_type == .other
+}
+
+fn assert_line_2_tokens(line_2 string, line_2_tokens []Token) {
+	assert line_2_tokens.len == 5
+	line_2_token_0 := line_2_tokens[0]
+	line_2_token_1 := line_2_tokens[1]
+	line_2_token_2 := line_2_tokens[2]
+	line_2_token_3 := line_2_tokens[3]
+	line_2_token_4 := line_2_tokens[4]
+
+	assert "${line_2[line_2_token_0.start..line_2_token_0.end]}" == "fn"
+	assert line_2_token_0.t_type == .other
+
+	assert "${line_2[line_2_token_1.start..line_2_token_1.end]}" == " "
+	assert line_2_token_1.t_type == .whitespace
+
+	assert "${line_2[line_2_token_2.start..line_2_token_2.end]}" == "main()"
+	assert line_2_token_2.t_type == .other
+
+	assert "${line_2[line_2_token_3.start..line_2_token_3.end]}" == " "
+	assert line_2_token_3.t_type == .whitespace
+
+	assert "${line_2[line_2_token_4.start..line_2_token_4.end]}" == "{"
+	assert line_2_token_4.t_type == .other
+}
+
