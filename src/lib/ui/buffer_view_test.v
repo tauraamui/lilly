8f30b73afb65d54f71e0e2c3141b7ad4dbcd4197
tauraamui <adampstringer@protonmail.com>
2025-04-14 16:38:36 +0100

    test: ensure lines for drawing from 10 upto max are rendered

--- src/lib/ui/buffer_view_test.v
+++ src/lib/ui/buffer_view_test.v
@@ -129,6 +129,7 @@ fn test_buffer_view_draws_lines_0_to_max_height_2() {
 	assert drawn_text.len == 42
 	assert set_fg_color.len == 42
 
+	// TODO(tauraamui) [14/04/2025]: need to assert against style draws as well
 	line_one_expected_drawn_data := [
 		DrawnText{ x: 1, y: 1, data: "1" }, DrawnText{ x: 4, y: 1, data: "This" },
 		DrawnText{ x: 8, y: 1, data: " " }, DrawnText{ x: 9, y: 1, data: "is" },
@@ -206,6 +207,83 @@ fn test_buffer_view_draws_lines_10_to_max_height() {
 		DrawnText{ x: 1, y: 10, data: "20" },
 		DrawnText{ x: 4, y: 10, data: "19 This is line 19 in the document" }
 	]
+}
+
+fn test_buffer_view_draws_lines_10_to_max_height_2() {
+	mut drawn_text := []DrawnText{}
+	mut drawn_text_ref := &drawn_text
+
+	mut set_fg_color := []draw.Color{}
+	mut set_fg_color_ref := &set_fg_color
+
+	mut drawn_rect := []DrawnRect{}
+	mut drawn_rect_ref := &drawn_rect
+
+	mut mock_ctx := MockContextable{
+		on_draw_cb: fn [mut drawn_text_ref] (x int, y int, text string) {
+			drawn_text_ref << DrawnText{ x: x, y: y, data: text }
+		}
+		on_draw_rect_cb: fn [mut drawn_rect_ref] (x int, y int, width int, height int) {
+			drawn_rect_ref << DrawnRect{ x: x, y: y, width: width, height: height }
+		}
+		on_set_fg_color_cb: fn [mut set_fg_color_ref] (c draw.Color) {
+			set_fg_color_ref << c
+		}
+	}
+
+	mut buf := buffer.Buffer.new("", false)
+	for i in 0..20 { buf.lines << "This is line ${i} in the document" }
+	buf_view := BufferView.new(&buf)
+
+	x := 0
+	y := 0
+	width := 100
+	height := 10
+	min_x := 0
+	from_line_num := 10
+
+	buf_view.draw_2(mut mock_ctx, x, y, width, height, from_line_num, min_x, 12)
+
+	// TODO(tauraamui) [14/04/2025]: need to assert against style draws as well
+	assert drawn_rect == [
+		DrawnRect{ x: 4, y: 3, width: 97, height: 1 }
+	]
+
+	assert drawn_text.len == 140
+	assert set_fg_color.len == 140
+
+	line_one_expected_drawn_data := [
+		DrawnText{ x: 1, y: 1, data: "11" }, DrawnText{ x: 5, y: 1, data: "This" },
+		DrawnText{ x: 9, y: 1, data: " " }, DrawnText{ x: 10, y: 1, data: "is" },
+		DrawnText{ x: 12, y: 1, data: " " }, DrawnText{ x: 13, y: 1, data: "line" },
+		DrawnText{ x: 17, y: 1, data: " " }, DrawnText{ x: 18, y: 1, data: "10" },
+		DrawnText{ x: 20, y: 1, data: " " }, DrawnText{ x: 21, y: 1, data: "in" },
+		DrawnText{ x: 23, y: 1, data: " " }, DrawnText{ x: 24, y: 1, data: "the" },
+		DrawnText{ x: 27, y: 1, data: " " }, DrawnText{ x: 28, y: 1, data: "document" },
+	]
+	assert drawn_text[..14] == line_one_expected_drawn_data
+
+	line_two_expected_drawn_data := [
+		DrawnText{ x: 1, y: 2, data: "12" }, DrawnText{ x: 5, y: 2, data: "This" },
+		DrawnText{ x: 9, y: 2, data: " " }, DrawnText{ x: 10, y: 2, data: "is" },
+		DrawnText{ x: 12, y: 2, data: " " }, DrawnText{ x: 13, y: 2, data: "line" },
+		DrawnText{ x: 17, y: 2, data: " " }, DrawnText{ x: 18, y: 2, data: "11" },
+		DrawnText{ x: 20, y: 2, data: " " }, DrawnText{ x: 21, y: 2, data: "in" },
+		DrawnText{ x: 23, y: 2, data: " " }, DrawnText{ x: 24, y: 2, data: "the" },
+		DrawnText{ x: 27, y: 2, data: " " }, DrawnText{ x: 28, y: 2, data: "document" },
+	]
+	assert drawn_text[14..28] == line_two_expected_drawn_data
+
+	line_three_expected_drawn_data := [
+		DrawnText{ x: 1, y: 3, data: "13" }, DrawnText{ x: 5, y: 3, data: "This" },
+		DrawnText{ x: 9, y: 3, data: " " }, DrawnText{ x: 10, y: 3, data: "is" },
+		DrawnText{ x: 12, y: 3, data: " " }, DrawnText{ x: 13, y: 3, data: "line" },
+		DrawnText{ x: 17, y: 3, data: " " }, DrawnText{ x: 18, y: 3, data: "12" },
+		DrawnText{ x: 20, y: 3, data: " " }, DrawnText{ x: 21, y: 3, data: "in" },
+		DrawnText{ x: 23, y: 3, data: " " }, DrawnText{ x: 24, y: 3, data: "the" },
+		DrawnText{ x: 27, y: 3, data: " " }, DrawnText{ x: 28, y: 3, data: "document" },
+	]
+	assert drawn_text[28..42] == line_three_expected_drawn_data
 }
 
 fn test_buffer_view_draws_lines_0_to_max_height_min_x_is_6() {
