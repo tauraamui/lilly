b3147b38b9d576f803cce1ae5ec8de57e357da52
tauraamui <adampstringer@protonmail.com>
2025-04-30 23:55:33 +0100

    test: ensure shrinking loses data in expected cells

--- src/lib/draw/tui_immediate_grid_test.v
+++ src/lib/draw/tui_immediate_grid_test.v
@@ -15,7 +15,7 @@ fn test_grid_setting_cell_oob() {
 	assert fail_msg == "x: 18, y: 0 is out of bounds"
 }
 
-fn test_grid_resize() {
+fn test_grid_resize_provides_ability_to_set_to_new_indexes() {
 	mut g := Grid.new(12, 12)!
 	g.set(0, 0, Cell{ data: rune(`\n`) })!
 	assert g.get(0, 0)! == Cell{ data: rune(`\n`) }
@@ -33,4 +33,20 @@ fn test_grid_resize() {
 
 	assert g.get(18, 16)! == Cell{ data: rune(`a`) }
 }
+
+fn test_grid_resize_shrinking_loses_data() {
+	mut g := Grid.new(24, 24)!
+	g.set(21, 15, Cell{ data: rune(`x`) })!
+	assert g.get(21, 15)! == Cell{ data: rune(`x`) }
+
+	mut fail_msg := ""
+	g.resize(10, 10) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	g.resize(24, 24) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	assert g.get(21, 15)! == Cell{ data: none }
+}
+
 
