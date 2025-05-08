394fabfb434c093bcf530e15069a5a747ee006b5
Adam Stringer <adampstringer@protonmail.com>
2025-05-08 22:13:58 +0100

    Feat/define initial immediate mode grid renderer (#367)
    * feat: define basic foundation for dynamically re-sizable frame grid
    
    * feat: manually guard width + height bounds for set + get
    
    * feat: implement resize to grow or shrink cell grid to current frame size
    
    * test: ensure shrinking loses data in expected cells
    
    * test: make sure that grid cells are set via draw text
    
    * test: ensure drawing text same len and place results in overwritten cell data
    
    * test: ensure only diff drawn text overwrites existing grid contents
    
    * feat: define method to acquire full range of rows from 1d grid
    
    * test: ensure method to fetch range of rows works
    
    * define pos field on ctx struct

---
+++ src/lib/draw/tui_immediate_grid.v
@@ -0,0 +1,206 @@
+module draw
+
+import term.ui as tui
+
+struct Pos {
+mut:
+	x int
+	y int
+}
+
+struct Grid {
+mut:
+	data      []Cell
+	prev_data []Cell
+	width     int
+	height    int
+}
+
+fn Grid.new(width int, height int) !Grid {
+	if width <= 0 || height <= 0 { return error("width and height must be positive") }
+	return Grid{ width: width, height: height, data: []Cell{ len: width * height } }
+}
+
+fn (mut grid Grid) set(x int, y int, c Cell) ! {
+	if x < 0 || x >= grid.width || y < 0 || y >= grid.height {
+		return error("x: ${x}, y: ${y} is out of bounds")
+	}
+	index := y * grid.width + x
+	grid.data[index] = c
+}
+
+fn (grid Grid) get(x int, y int) !Cell {
+	if x < 0 || x >= grid.width || y < 0 || y >= grid.height {
+		return error("x: ${x}, y: ${y} is out of bounds")
+	}
+	index := y * grid.width + x
+	return grid.data[index]
+}
+
+fn (grid Grid) get_rows(min int, max int) ![][]Cell {
+	if min < 0 || min >= grid.data.len || max < 0 || max >= grid.data.len || min > max {
+		return error("invalid row range")
+	}
+	rows_in_range := max - min + 1
+	mut result := [][]Cell{ len: rows_in_range }
+
+	for i in 0..rows_in_range {
+		current_row := min + i
+		start_index := current_row * grid.width
+		end_index := start_index + grid.width
+		result[i] = grid.data[start_index..end_index]
+	}
+
+	return result
+}
+
+fn (mut grid Grid) resize(width int, height int) ! {
+	if width <= 0 || height <= 0 { return error("width and height must be positive") }
+	if height == grid.height && width == grid.width {
+		return
+	}
+
+	mut new_data := []Cell{ len: width * height }
+	overlap_rows := int_min(grid.height, height)
+	overlap_cols := int_min(grid.width, width)
+
+	for i in 0..overlap_rows {
+		for j in 0..overlap_cols {
+			old_index := i * grid.width + j
+			new_index := i * width + j
+			new_data[new_index] = grid.data[old_index]
+		}
+	}
+
+	grid.width = width
+	grid.height = height
+	grid.data = new_data
+}
+
+struct Cell {
+	data     ?rune
+	fg_color Color
+	bg_color Color
+}
+
+fn (cell Cell) str() string {
+	r := cell.data or { return "none" }
+	return r.str()
+}
+
+struct ImmediateContext {
+	render_debug bool
+mut:
+	ref         &tui.Context
+	data        Grid
+	cursor_pos  Pos
+	hide_cursor bool
+}
+
+type Runner = fn () !
+
+pub fn new_immediate_context(cfg Config) (&Contextable, Runner) {
+	mut ctx := ImmediateContext{
+		render_debug: cfg.render_debug
+		ref: tui.init(
+			user_data: cfg.user_data
+			event_fn:  fn [cfg] (e &tui.Event, app voidptr) {
+				cfg.event_fn(Event{e}, app)
+			}
+			frame_fn:             cfg.frame_fn
+			capture_events:       cfg.capture_events
+			use_alternate_buffer: cfg.use_alternate_buffer
+			frame_rate: 30
+		)
+	}
+	ctx.setup_grid() or { panic("unable to init grid -> ${err}") }
+	return ctx, unsafe { ctx.run }
+}
+
+fn (mut ctx ImmediateContext) setup_grid() ! {
+	ctx.data = Grid.new(ctx.window_width(), ctx.window_height())!
+}
+
+fn (mut ctx ImmediateContext) rate_limit_draws() bool {
+	return true
+}
+
+fn (mut ctx ImmediateContext) render_debug() bool { return ctx.render_debug }
+
+fn (mut ctx ImmediateContext) window_width() int {
+	return 100
+}
+
+fn (mut ctx ImmediateContext) window_height() int {
+	return 100
+}
+
+fn (mut ctx ImmediateContext) set_cursor_position(x int, y int) {
+	ctx.cursor_pos = Pos{ x: x, y: y }
+}
+
+fn (mut ctx ImmediateContext) show_cursor() {
+	ctx.ref.show_cursor()
+	ctx.hide_cursor = false
+}
+
+fn (mut ctx ImmediateContext) hide_cursor() {
+	ctx.hide_cursor = true
+}
+
+fn (mut ctx ImmediateContext) draw_text(x int, y int, text string) {
+	ctx.set_cursor_position(x, y)
+	ctx.write(text)
+}
+
+fn (mut ctx ImmediateContext) write(c string) {
+	cursor_pos := ctx.cursor_pos
+	for i, c_char in c.runes() {
+		ctx.data.set(cursor_pos.x + i, cursor_pos.y, Cell{ data: c_char }) or { break }
+	}
+}
+
+fn (mut ctx ImmediateContext) draw_rect(x int, y int, width int, height int) {
+	ctx.ref.draw_rect(x, y, x + (width - 1), y + (height - 1))
+}
+
+fn (mut ctx ImmediateContext) draw_point(x int, y int) {
+	ctx.ref.draw_point(x, y)
+}
+
+fn (mut ctx ImmediateContext) bold() {
+	ctx.ref.bold()
+}
+
+fn (mut ctx ImmediateContext) set_color(c Color) {
+	ctx.ref.set_color(tui.Color{ r: c.r, g: c.g, b: c.b })
+}
+
+fn (mut ctx ImmediateContext) set_bg_color(c Color) {
+	ctx.ref.set_bg_color(tui.Color{ r: c.r, g: c.g, b: c.b })
+}
+
+fn (mut ctx ImmediateContext) reset_color() {
+	ctx.ref.reset_color()
+}
+
+fn (mut ctx ImmediateContext) reset_bg_color() {
+	ctx.ref.reset_bg_color()
+}
+
+fn (mut ctx ImmediateContext) reset() {
+	ctx.ref.reset()
+}
+
+fn (mut ctx ImmediateContext) run() ! {
+	return ctx.ref.run()
+}
+
+fn (mut ctx ImmediateContext) clear() {
+	ctx.ref.clear()
+}
+
+fn (mut ctx ImmediateContext) flush() {
+	ctx.ref.flush()
+}
+
---
+++ src/lib/draw/tui_immediate_grid_test.v
@@ -0,0 +1,198 @@
+module draw
+
+fn test_cell_data_to_string() {
+	cell := Cell{ data: none }
+	assert cell.str() == "none"
+}
+
+fn test_cell_data_single_rune_to_string() {
+	cell := Cell{ data: rune(`x`) }
+	assert cell.str() == "x"
+}
+
+fn test_grid_setting_cell_reading_cell() {
+	mut g := Grid.new(12, 12)!
+	g.set(0, 0, Cell{ data: rune(`\n`) })!
+	assert g.get(0, 0)! == Cell{ data: rune(`\n`) }
+}
+
+fn test_grid_setting_cell_oob() {
+	mut g := Grid.new(12, 12)!
+	mut fail_msg := ""
+	g.set(18, 0, Cell{ data: rune(`\n`) }) or { fail_msg = err.str() }
+	assert fail_msg == "x: 18, y: 0 is out of bounds"
+	g.get(18, 0) or { fail_msg = err.str() }
+	assert fail_msg == "x: 18, y: 0 is out of bounds"
+}
+
+fn test_grid_resize_provides_ability_to_set_to_new_indexes() {
+	mut g := Grid.new(12, 12)!
+	g.set(0, 0, Cell{ data: rune(`\n`) })!
+	assert g.get(0, 0)! == Cell{ data: rune(`\n`) }
+
+	mut fail_msg := ""
+	g.set(18, 16, Cell{ data: rune(`\n`) }) or { fail_msg = err.str() }
+	assert fail_msg == "x: 18, y: 16 is out of bounds"
+
+	fail_msg = ""
+	g.resize(24, 24) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	g.set(18, 16, Cell{ data: rune(`a`) }) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	assert g.get(18, 16)! == Cell{ data: rune(`a`) }
+}
+
+fn test_grid_resize_shrinking_loses_data() {
+	mut g := Grid.new(24, 24)!
+	g.set(21, 15, Cell{ data: rune(`x`) })!
+	assert g.get(21, 15)! == Cell{ data: rune(`x`) }
+
+	mut fail_msg := ""
+	g.resize(10, 10) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	g.resize(24, 24) or { fail_msg = err.str() }
+	assert fail_msg == ""
+
+	assert g.get(21, 15)! == Cell{ data: none }
+}
+
+fn test_immediate_context_draw_text_sets_cells() {
+	mut ctx := ImmediateContext{
+		ref: unsafe { nil }
+	}
+	ctx.setup_grid()!
+
+	ctx.draw_text(10, 10, "This is a line of text")
+	assert ctx.data.get(9, 10)!  == Cell{ data: none }
+	assert ctx.data.get(10, 10)! == Cell{ data: rune(`T`) }
+	assert ctx.data.get(11, 10)! == Cell{ data: rune(`h`) }
+	assert ctx.data.get(12, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(13, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(14, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(15, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(16, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(17, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(18, 10)! == Cell{ data: rune(`a`) }
+	assert ctx.data.get(19, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(20, 10)! == Cell{ data: rune(`l`) }
+	assert ctx.data.get(21, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(22, 10)! == Cell{ data: rune(`n`) }
+	assert ctx.data.get(23, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(24, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(25, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(26, 10)! == Cell{ data: rune(`f`) }
+	assert ctx.data.get(27, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(28, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(29, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(30, 10)! == Cell{ data: rune(`x`) }
+	assert ctx.data.get(31, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(32, 10)! == Cell{ data: none }
+}
+
+fn test_immediate_context_draw_text_sets_cells_get_rows() {
+	mut ctx := ImmediateContext{
+		ref: unsafe { nil }
+	}
+	ctx.setup_grid()!
+
+	ctx.draw_text(10, 10, "This is a line of text")
+	cells := ctx.data.get_rows(10, 10)!
+	assert cells == []
+	assert ctx.data.get(9, 10)!  == Cell{ data: none }
+	assert ctx.data.get(10, 10)! == Cell{ data: rune(`T`) }
+	assert ctx.data.get(11, 10)! == Cell{ data: rune(`h`) }
+	assert ctx.data.get(12, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(13, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(14, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(15, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(16, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(17, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(18, 10)! == Cell{ data: rune(`a`) }
+	assert ctx.data.get(19, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(20, 10)! == Cell{ data: rune(`l`) }
+	assert ctx.data.get(21, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(22, 10)! == Cell{ data: rune(`n`) }
+	assert ctx.data.get(23, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(24, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(25, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(26, 10)! == Cell{ data: rune(`f`) }
+	assert ctx.data.get(27, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(28, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(29, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(30, 10)! == Cell{ data: rune(`x`) }
+	assert ctx.data.get(31, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(32, 10)! == Cell{ data: none }
+}
+
+fn test_immediate_context_multiple_draw_text_sets_cells_overwrites() {
+	mut ctx := ImmediateContext{
+		ref: unsafe { nil }
+	}
+	ctx.setup_grid()!
+
+	ctx.draw_text(10, 10, "This is a line of text")
+	ctx.draw_text(10, 10, "Also some other stuff!")
+	assert ctx.data.get(9, 10)!  == Cell{ data: none }
+	assert ctx.data.get(10, 10)! == Cell{ data: rune(`A`) }
+	assert ctx.data.get(11, 10)! == Cell{ data: rune(`l`) }
+	assert ctx.data.get(12, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(13, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(14, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(15, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(16, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(17, 10)! == Cell{ data: rune(`m`) }
+	assert ctx.data.get(18, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(19, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(20, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(21, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(22, 10)! == Cell{ data: rune(`h`) }
+	assert ctx.data.get(23, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(24, 10)! == Cell{ data: rune(`r`) }
+	assert ctx.data.get(25, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(26, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(27, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(28, 10)! == Cell{ data: rune(`u`) }
+	assert ctx.data.get(29, 10)! == Cell{ data: rune(`f`) }
+	assert ctx.data.get(30, 10)! == Cell{ data: rune(`f`) }
+	assert ctx.data.get(31, 10)! == Cell{ data: rune(`!`) }
+	assert ctx.data.get(32, 10)! == Cell{ data: none }
+}
+
+fn test_immediate_context_multiple_draw_text_sets_cells_overwrites_only_cells_that_overlap() {
+	mut ctx := ImmediateContext{
+		ref: unsafe { nil }
+	}
+	ctx.setup_grid()!
+
+	ctx.draw_text(10, 10, "This is a line of text")
+	ctx.draw_text(10, 10, "Not as much")
+	assert ctx.data.get(9, 10)!  == Cell{ data: none }
+	assert ctx.data.get(10, 10)! == Cell{ data: rune(`N`) }
+	assert ctx.data.get(11, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(12, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(13, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(14, 10)! == Cell{ data: rune(`a`) }
+	assert ctx.data.get(15, 10)! == Cell{ data: rune(`s`) }
+	assert ctx.data.get(16, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(17, 10)! == Cell{ data: rune(`m`) }
+	assert ctx.data.get(18, 10)! == Cell{ data: rune(`u`) }
+	assert ctx.data.get(19, 10)! == Cell{ data: rune(`c`) }
+	assert ctx.data.get(20, 10)! == Cell{ data: rune(`h`) }
+	assert ctx.data.get(21, 10)! == Cell{ data: rune(`i`) }
+	assert ctx.data.get(22, 10)! == Cell{ data: rune(`n`) }
+	assert ctx.data.get(23, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(24, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(25, 10)! == Cell{ data: rune(`o`) }
+	assert ctx.data.get(26, 10)! == Cell{ data: rune(`f`) }
+	assert ctx.data.get(27, 10)! == Cell{ data: rune(` `) }
+	assert ctx.data.get(28, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(29, 10)! == Cell{ data: rune(`e`) }
+	assert ctx.data.get(30, 10)! == Cell{ data: rune(`x`) }
+	assert ctx.data.get(31, 10)! == Cell{ data: rune(`t`) }
+	assert ctx.data.get(32, 10)! == Cell{ data: none }
+}
+
+
