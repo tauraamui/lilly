2b3e48d1dda69dedd89276439ee312287e40606a
tauraamui <adampstringer@protonmail.com>
2025-05-18 15:21:37 +0100

    test: ensure inline or block is persisted in clipboard

--- src/lib/clipboardv3/clipboard_test.v
+++ src/lib/clipboardv3/clipboard_test.v
@@ -2,12 +2,21 @@ module clipboardv3
 
 fn test_clipboard_native_implementation() ! {
 	$if darwin {
-		ct := ContentType.inline
 		clipboard := new() or { return error("unable to acquire clipboard") }
 		clipboard.set_content(ClipboardContent{ data: "This is copied text!", t_type: .inline })
 		assert clipboard.get_content() or {
 			return error("failed to get contents")
 		} == ClipboardContent{ data: "This is copied text!", t_type: .inline }
+	}
+}
+
+fn test_clipboard_native_implementation_sets_type_to_block() ! {
+	$if darwin {
+		clipboard := new() or { return error("unable to acquire clipboard") }
+		clipboard.set_content(ClipboardContent{ data: "This is copied text!", t_type: .block })
+		assert clipboard.get_content() or {
+			return error("failed to get contents")
+		} == ClipboardContent{ data: "This is copied text!", t_type: .block }
 	}
 }
 
