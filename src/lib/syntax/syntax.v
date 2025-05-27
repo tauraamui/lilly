782141de13d12d53e6cc1ff529e131c2f14c71cb
tauraamui <adampstringer@protonmail.com>
2025-05-28 00:22:16 +0100

    format: adjust positioning of char struct defs to be lined up

--- src/lib/syntax/syntax.v
+++ src/lib/syntax/syntax.v
@@ -14,20 +14,20 @@ const builtin_python_syntax = $embed_file('../../syntax/python.syntax').to_strin
 const builtin_perl_syntax = $embed_file('../../syntax/perl.syntax').to_string()
 
 pub const colors := {
-	TokenType.identifier: draw.Color{200, 200, 235}
-	.operator:            draw.Color{200, 200, 235}
-	.string:              draw.Color{87, 215, 217}
-	.comment:             draw.Color{130, 130, 130}
-	.comment_start:       draw.Color{200, 200, 235}
-	.comment_end:         draw.Color{200, 200, 235}
-	.block_start:         draw.Color{200, 200, 235}
-	.block_end:           draw.Color{200, 200, 235}
-	.number:              draw.Color{220, 110, 110}
-	.whitespace:          draw.Color{200, 200, 235}
-	.keyword:             draw.Color{ 255, 126, 192 }
-	.literal:             draw.Color{ 87, 215, 217 }
+	TokenType.identifier: draw.Color{ 200, 200, 235 }
+	.operator:            draw.Color{ 200, 200, 235 }
+	.string:              draw.Color{ 87,  215, 217 }
+	.comment:             draw.Color{ 130, 130, 130 }
+	.comment_start:       draw.Color{ 200, 200, 235 }
+	.comment_end:         draw.Color{ 200, 200, 235 }
+	.block_start:         draw.Color{ 200, 200, 235 }
+	.block_end:           draw.Color{ 200, 200, 235 }
+	.number:              draw.Color{ 220, 110, 110 }
+	.whitespace:          draw.Color{ 200, 200, 235 }
+	.keyword:             draw.Color{ 255, 95,  175 }
+	.literal:             draw.Color{ 87,  215, 217 }
 	.builtin:             draw.Color{ 130, 144, 250 }
-	.other:               draw.Color{200, 200, 235}
+	.other:               draw.Color{ 200, 200, 235 }
 }
 
 pub struct Syntax {
