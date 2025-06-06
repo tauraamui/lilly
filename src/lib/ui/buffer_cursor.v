7d2baa22ea772c3650f2f3fc57cc4b0a2344a14a
tauraamui <adampstringer@protonmail.com>
2025-06-06 19:34:48 +0100

    wip: refactoring cursor selection implementation stuff

--- src/lib/ui/buffer_cursor.v
+++ src/lib/ui/buffer_cursor.v
@@ -7,9 +7,20 @@ pub mut:
 }
 
 pub struct BufferCursor {
+mut:
+	sel_start_pos       ?CursorPos
 pub mut:
+	selection_start_pos CursorPos
 	pos                 CursorPos
-	selection_start_pos ?CursorPos
+}
+
+pub fn (cursor BufferCursor) y_within_selection(line_y int) bool {
+	if sel_pos := cursor.sel_start_pos {
+		start := match true { sel_pos.y < cursor.pos.y { sel_pos.y } else { cursor.pos.y } }
+		end   := match true { cursor.pos.y > sel_pos.y { cursor.pos.y } else { sel_pos.y } }
+		return line_y >= start && line_y <= end
+	}
+	return false
 }
 
 pub fn (cursor BufferCursor) line_is_within_selection(line_y int) bool {
@@ -27,6 +38,10 @@ pub fn (cursor BufferCursor) line_is_within_selection(line_y int) bool {
 	return line_y >= start && line_y <= end
 }
 
+pub fn (cursor BufferCursor) sel_start() ?CursorPos {
+	return ?CursorPos(none)
+}
+
 pub fn (cursor BufferCursor) selection_start() CursorPos {
 	if cursor.selection_start_pos.y == cursor.pos.y {
 		if cursor.selection_start_pos.x == cursor.pos.x {
@@ -42,6 +57,10 @@ pub fn (cursor BufferCursor) selection_start() CursorPos {
 	} else {
 		return cursor.pos
 	}
+}
+
+pub fn (cursor BufferCursor) sel_end() ?CursorPos {
+	return ?CursorPos(none)
 }
 
 pub fn (cursor BufferCursor) selection_end() CursorPos {
@@ -62,13 +81,14 @@ pub fn (cursor BufferCursor) selection_end() CursorPos {
 }
 
 pub fn (cursor BufferCursor) selection_active() bool {
-	if selection_start := cursor.selection_start_pos {
-		return selection_start.x >= 0 && selection_start.y >= 0
-	}
 	return false
 }
 
+pub fn (mut cursor BufferCursor) set_selection(sel CursorPos) {
+	cursor.sel_start_pos = sel
+}
+
 pub fn (mut cursor BufferCursor) clear_selection() {
-	cursor.selection_start_pos = ?CursorPos(none)
+	cursor.sel_start_pos = ?CursorPos(none)
 }
 
--- src/lib/ui/buffer_cursor_test.v
+++ src/lib/ui/buffer_cursor_test.v
@@ -1,7 +1,23 @@
 module ui
 
-fn test_cursor_check_if_line_within_selection() {
-	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 10 } }
-	assert cursor.selection_active() == false
+fn test_cursor_check_if_line_within_selection_in_order() {
+	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 3 }, sel_start_pos: CursorPos{ 0, 10 } }
+	assert cursor.y_within_selection(8)
 }
+
+fn test_cursor_check_if_line_within_selection_in_order_oob() {
+	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 3 }, sel_start_pos: CursorPos{ 0, 10 } }
+	assert cursor.y_within_selection(24) == false
+}
+
+fn test_cursor_check_if_line_within_selection_in_reverse_order() {
+	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 10 }, sel_start_pos: CursorPos{ 0, 3 } }
+	assert cursor.y_within_selection(8)
+}
+
+fn test_cursor_check_if_line_within_selection_in_reverse_order_oob() {
+	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 10 }, sel_start_pos: CursorPos{ 0, 3 } }
+	assert cursor.y_within_selection(24) == false
+}
+
 
