0f4be8245ee042ed1f37a93f376c52cff1d00a9a
tauraamui <adampstringer@protonmail.com>
2025-06-07 19:04:06 +0100

    test: ensure same line but x pos is resolved correctly

--- src/lib/ui/buffer_cursor_test.v
+++ src/lib/ui/buffer_cursor_test.v
@@ -37,19 +37,27 @@ fn test_cursor_check_start_pos_is_cursor_pos_if_start_is_after() {
 	// start x and y is more than cursor x and y
 	cursor = BufferCursor{ pos: CursorPos{ x: 33, y: 12 }, sel_start_pos: CursorPos{ x: 57, y: 44 } }
 	assert cursor.sel_start()? == CursorPos{ x: 33, y: 12 }
+
+	// start x is more y is the same as cursor x and y
+	cursor = BufferCursor{ pos: CursorPos{ x: 33, y: 12 }, sel_start_pos: CursorPos{ x: 57, y: 12 } }
+	assert cursor.sel_start()? == CursorPos{ x: 33, y: 12 }
 }
 
 fn test_cursor_check_start_pos_is_start_pos_if_cursor_is_after() {
-	// start y is more than cursor y
+	// cursor y is more than start y
 	mut cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 15 }, sel_start_pos: CursorPos{ x: 0, y: 10 } }
 	assert cursor.sel_start()? == CursorPos{ x: 0, y: 10 }
 
-	// start x is more than cursor x
+	// cursor x is more than start x
 	cursor = BufferCursor{ pos: CursorPos{ x: 10, y: 0 }, sel_start_pos: CursorPos{ x: 8, y: 0 } }
 	assert cursor.sel_start()? == CursorPos{ x: 8, y: 0 }
 
-	// start x and y is more than cursor x and y
+	// cursor x and y is more than start x and y
 	cursor = BufferCursor{ pos: CursorPos{ x: 57, y: 44 }, sel_start_pos: CursorPos{ x: 33, y: 12 } }
 	assert cursor.sel_start()? == CursorPos{ x: 33, y: 12 }
+
+	// cursor x is more y is the same as start x and y
+	cursor = BufferCursor{ pos: CursorPos{ x: 57, y: 44 }, sel_start_pos: CursorPos{ x: 33, y: 44 } }
+	assert cursor.sel_start()? == CursorPos{ x: 33, y: 44 }
 }
 
