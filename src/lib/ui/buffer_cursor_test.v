9a1e0102a6f8aafcd1863d3207cd2683288ab40a
tauraamui <adampstringer@protonmail.com>
2025-06-12 01:01:37 +0100

    test: ensure resolved selection span resolves correctly for same line

--- src/lib/ui/buffer_cursor_test.v
+++ src/lib/ui/buffer_cursor_test.v
@@ -33,6 +33,11 @@ fn test_cursor_resolve_line_selection_span_if_visual_and_y_in_selection_start_x_
 	assert cursor.resolve_line_selection_span(.visual, 30, 10) == SelectionSpan{ min_x: 0, max_x: 25, full: false }
 }
 
+fn test_cursor_resolve_line_selection_span_if_visual_and_y_in_selection_starts_ends_on_same_line() {
+	mut cursor := BufferCursor{ pos: CursorPos{ x: 20, y: 3 }, sel_start_pos: CursorPos{ 10, 3 } }
+	assert cursor.resolve_line_selection_span(.visual, 30, 3) == SelectionSpan{ min_x: 10, max_x: 20, full: false }
+}
+
 fn test_cursor_check_if_line_within_selection_in_order() {
 	cursor := BufferCursor{ pos: CursorPos{ x: 0, y: 3 }, sel_start_pos: CursorPos{ 0, 10 } }
 	assert cursor.y_within_selection(8)
