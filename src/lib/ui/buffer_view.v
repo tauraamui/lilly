835aa50502620fcf3bc035c94c4480a187b84c32
tauraamui <adampstringer@protonmail.com>
2025-06-18 14:44:01 +0100

    refactor: make logic gate pre segment splitting single statement

--- src/lib/ui/buffer_view.v
+++ src/lib/ui/buffer_view.v
@@ -288,23 +288,21 @@ fn render_segment_in_visual_mode(
 		return render_segment_in_visual_mode_segment_does_not_overlap_with_selection(mut ctx, segment_bounds, segment, fg_color, x, y)
 	}
 
-	if selection_span.min_x < segment_bounds.start {
-		if selection_span.max_x < segment_bounds.end {
-			i := selection_span.max_x - segment_bounds.start
-			selected_part_of_segment := segment.runes()[..i].string()
-			rest_of_segment          := segment.runes()[i..].string()
+	if selection_span.min_x < segment_bounds.start && selection_span.max_x < segment_bounds.end {
+		i := selection_span.max_x - segment_bounds.start
+		selected_part_of_segment := segment.runes()[..i].string()
+		rest_of_segment          := segment.runes()[i..].string()
 
-			bg_color := ctx.theme().selection_highlight_color
-			ctx.set_bg_color(draw.Color{ bg_color.r, bg_color.g, bg_color.b })
-			ctx.draw_text(x, y, selected_part_of_segment)
-			ctx.reset_bg_color()
+		bg_color := ctx.theme().selection_highlight_color
+		ctx.set_bg_color(draw.Color{ bg_color.r, bg_color.g, bg_color.b })
+		ctx.draw_text(x, y, selected_part_of_segment)
+		ctx.reset_bg_color()
 
-			visable_length := utf8_str_visible_length(selected_part_of_segment)
-			ctx.set_color(draw.Color{ fg_color.r, fg_color.g, fg_color.b })
-			ctx.draw_text(x + visable_length, y, rest_of_segment)
+		visable_length := utf8_str_visible_length(selected_part_of_segment)
+		ctx.set_color(draw.Color{ fg_color.r, fg_color.g, fg_color.b })
+		ctx.draw_text(x + visable_length, y, rest_of_segment)
 
-			return visable_length + utf8_str_visible_length(rest_of_segment)
-		}
+		return visable_length + utf8_str_visible_length(rest_of_segment)
 	}
 
 	return 0
