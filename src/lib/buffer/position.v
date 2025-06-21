1bf9481f987782d7f69ae63827ce64cafe8d8cbb
tauraamui <adampstringer@protonmail.com>
2025-06-21 20:47:57 +0100

    feat: implement being able to inplace add position by distance

--- src/lib/buffer/position.v
+++ src/lib/buffer/position.v
@@ -18,6 +18,14 @@ pub fn (p Position) add(d Distance) Position {
 	}
 }
 
+pub fn (mut p Position) apply(d Distance) {
+	offset := if d.lines > 0 { d.offset } else { p.offset + d.offset }
+	p = Position {
+		line: p.line + d.lines
+		offset: offset
+	}
+}
+
 fn (a Position) < (b Position) bool {
 	return match true {
 		a.line < b.line     { true }
