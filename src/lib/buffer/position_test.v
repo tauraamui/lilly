1d67451bc9bd2e521374c9ec4818b63ef80a6f24
tauraamui <adampstringer@protonmail.com>
2025-06-21 20:42:17 +0100

    test: ensure position operators work correctly

--- src/lib/buffer/position_test.v
+++ src/lib/buffer/position_test.v
@@ -21,3 +21,47 @@ fn test_add_with_some_line_distance() {
 	assert pos.add(dist) == Position{ line: 13, offset: 4 }
 }
 
+fn test_less_than_works_when_position_a_less_then_b() {
+	earlier_position := Position{ line: 2, offset: 20 }
+	later_position   := Position{ line: 3, offset: 10 }
+
+	assert earlier_position < later_position
+}
+
+fn test_less_than_works_when_position_b_more_then_a() {
+	earlier_position := Position{ line: 2, offset: 20 }
+	later_position   := Position{ line: 3, offset: 10 }
+
+	assert later_position < earlier_position == false
+}
+
+fn test_more_than_works_when_position_b_more_then_a() {
+	earlier_position := Position{ line: 2, offset: 20 }
+	later_position   := Position{ line: 3, offset: 10 }
+
+	assert later_position > earlier_position
+}
+
+fn test_more_than_works_when_position_a_less_then_b() {
+	earlier_position := Position{ line: 2, offset: 20 }
+	later_position   := Position{ line: 3, offset: 10 }
+
+	assert earlier_position > later_position == false
+}
+
+fn test_compare_works_when_lines_and_offsets_are_equal() {
+	position_a := Position{ line: 11, offset: 3 }
+	position_b := Position{ line: 11, offset: 3 }
+
+	assert position_a <= position_b
+	assert position_a >= position_b
+	assert position_a == position_b
+}
+
+fn test_compare_works_when_lines_and_offsets_are_not_equal() {
+	position_a := Position{ line: 11, offset: 3 }
+	position_b := Position{ line: 8, offset: 3 }
+
+	assert position_a != position_b
+}
+
