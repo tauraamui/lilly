cf1d5571b29e89175297f4950b71640277b457cc
tauraamui <adampstringer@protonmail.com>
2025-06-21 19:34:53 +0100

    test: ensure variation of position add distance is working

--- src/lib/buffer/position_test.v
+++ src/lib/buffer/position_test.v
@@ -7,3 +7,17 @@ fn test_add_with_zero_line_distance() {
 	assert pos.add(dist) == Position{ line: 1, offset: 7 }
 }
 
+fn test_add_with_zero_line_distance_diff_offset() {
+	mut pos := Position{ line: 1, offset: 3 }
+	dist    := Distance{ lines: 0, offset: 19 }
+
+	assert pos.add(dist) == Position{ line: 1, offset: 22 }
+}
+
+fn test_add_with_some_line_distance() {
+	mut pos := Position{ line: 1, offset: 3 }
+	dist    := Distance{ lines: 12, offset: 4 }
+
+	assert pos.add(dist) == Position{ line: 13, offset: 4 }
+}
+
