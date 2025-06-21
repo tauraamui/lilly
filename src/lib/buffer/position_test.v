8caa306e98ca920d772d6644c8e01f42a2c006aa
tauraamui <adampstringer@protonmail.com>
2025-06-21 20:48:06 +0100

    test: ensure in place position add distance works

--- src/lib/buffer/position_test.v
+++ src/lib/buffer/position_test.v
@@ -7,6 +7,14 @@ fn test_add_with_zero_line_distance() {
 	assert pos.add(dist) == Position{ line: 1, offset: 7 }
 }
 
+fn test_apply_with_zero_line_distance() {
+	mut pos := Position{ line: 1, offset: 3 }
+	dist    := Distance{ lines: 0, offset: 4 }
+
+	pos.apply(dist)
+	assert pos == Position{ line: 1, offset: 7 }
+}
+
 fn test_add_with_zero_line_distance_diff_offset() {
 	mut pos := Position{ line: 1, offset: 3 }
 	dist    := Distance{ lines: 0, offset: 19 }
