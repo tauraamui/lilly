dc39c9cac6d25816eb722075b6d54ec27c38e04b
tauraamui <adampstringer@protonmail.com>
2025-06-21 20:58:33 +0100

    feat: implement range type with useful constructor

---
+++ src/lib/buffer/range.v
@@ -0,0 +1,20 @@
+module buffer
+
+@[noinit]
+pub struct Range {
+pub:
+	start Position
+	end   Position
+}
+
+fn Range.new(start Position, end Position) Range {
+	if start > end {
+		return Range{ start: end, end: start }
+	}
+	return Range{ start, end }
+}
+
+pub fn (range Range) includes(position Position) bool {
+	return position >= range.start && position < range.end
+}
+
