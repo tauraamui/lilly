586833c6b0e557ed7a8a2dc47f3bb7ea6f100275
tauraamui <adampstringer@protonmail.com>
2025-06-23 15:07:17 +0100

    test: ensure if inserting on line higher than current max insert the diff

--- src/lib/buffer/line_buffer_test.v
+++ src/lib/buffer/line_buffer_test.v
@@ -11,5 +11,12 @@ fn test_line_buffer_insert_text_with_initially_empty_data() {
 }
 
 fn test_line_buffer_insert_text_at_line_higher_than_current_data() {
+	mut line_buf := LineBuffer{
+		lines: ["1. first line", "2. second line", "3. third line", "4. fourth line"]
+	}
+
+	new_pos := line_buf.insert_text(Position.new(7, 0), "8. eighth line")?
+	assert new_pos == Position.new(7, 14)
+	assert line_buf.lines == ["1. first line", "2. second line", "3. third line", "4. fourth line", "", "", "", "8. eighth line"]
 }
 
