26373bc29b0f51f9bfe93de3475cbe3484b672c8
tauraamui <adampstringer@protonmail.com>
2025-06-23 15:23:27 +0100

    fix: return new clamped position with new inserted data applied

--- src/lib/buffer/line_buffer.v
+++ src/lib/buffer/line_buffer.v
@@ -16,11 +16,13 @@ pub fn (mut l_buffer LineBuffer) insert_text(pos Position, s string) ?Position {
 	mut clamped_offset := if pos.offset > line_content.len { line_content.len } else { pos.offset }
 	if clamped_offset > line_content.runes().len { return Position.new(pos.line, clamped_offset) }
 
-	pre_line_content  := line_content.runes()[..pos.offset].string()
-	post_line_content := line_content.runes()[pos.offset..line_content.runes().len].string()
+	pre_line_content  := line_content.runes()[..clamped_offset].string()
+	post_line_content := line_content.runes()[clamped_offset..line_content.runes().len].string()
 
 	l_buffer.lines[pos.line] = "${pre_line_content}${s}${post_line_content}"
 
-	return pos.add(Distance{ lines: 0, offset: s.runes().len })
+	clamped_pos := Position.new(pos.line, clamped_offset)
+
+	return clamped_pos.add(Distance{ lines: 0, offset: s.runes().len })
 }
 
