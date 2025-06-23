f7910cc6ec5647d02d38f4fdddd4d50e12247074
tauraamui <adampstringer@protonmail.com>
2025-06-23 15:28:36 +0100

    test: if inserting further up line than exists clamp offset to line len

--- src/lib/buffer/line_buffer_test.v
+++ src/lib/buffer/line_buffer_test.v
@@ -20,6 +20,15 @@ fn test_line_buffer_insert_text_at_offset_higher_than_current_line_size() {
 	assert line_buf.lines == ["1. first line", "2. second line text at end of second line", "3. third line", "4. fourth line"]
 }
 
+fn test_line_buffer_insert_text_at_offset_higher_than_0_when_current_line_is_empty() {
+	mut line_buf := LineBuffer{
+		lines: ["1. first line", "", "3. third line", "4. fourth line"]
+	}
+
+	new_pos := line_buf.insert_text(Position.new(1, 140), " text at end of second line")?
+	assert new_pos == Position.new(1, 27)
+	assert line_buf.lines == ["1. first line", " text at end of second line", "3. third line", "4. fourth line"]
+}
 
 fn test_line_buffer_insert_text_at_line_higher_than_current_data() {
 	mut line_buf := LineBuffer{
