97b82c72db1d1e607cd7aeacde58d578b08db88c
tauraamui <adampstringer@protonmail.com>
2025-06-23 15:12:04 +0100

    test: ensure inserting at start and middle of existing line works

--- src/lib/buffer/line_buffer_test.v
+++ src/lib/buffer/line_buffer_test.v
@@ -20,3 +20,24 @@ fn test_line_buffer_insert_text_at_line_higher_than_current_data() {
 	assert line_buf.lines == ["1. first line", "2. second line", "3. third line", "4. fourth line", "", "", "", "8. eighth line"]
 }
 
+fn test_line_buffer_insert_text_at_start_of_existing_line() {
+	mut line_buf := LineBuffer{
+		lines: ["1. first line", "2. second line", "3. third line", "4. fourth line"]
+	}
+
+	new_pos := line_buf.insert_text(Position.new(1, 0), "prefix text on second line")?
+	assert new_pos == Position.new(1, 26)
+	assert line_buf.lines == ["1. first line", "prefix text on second line2. second line", "3. third line", "4. fourth line"]
+}
+
+fn test_line_buffer_insert_text_at_middle_of_existing_line() {
+	mut line_buf := LineBuffer{
+		lines: ["1. first line", "2. second line", "3. third line", "4. fourth line"]
+	}
+
+	new_pos := line_buf.insert_text(Position.new(1, 7), " middle text within second line ")?
+	assert new_pos == Position.new(1, 39)
+	assert line_buf.lines == ["1. first line", "2. seco middle text within second line nd line", "3. third line", "4. fourth line"]
+}
+
+
