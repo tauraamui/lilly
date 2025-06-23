6f26c496284a19567a379b92ad5b6c2907ab7036
tauraamui <adampstringer@protonmail.com>
2025-06-23 15:23:14 +0100

    test: make sure offset is clamped down to line len if exceeds

--- src/lib/buffer/line_buffer_test.v
+++ src/lib/buffer/line_buffer_test.v
@@ -10,6 +10,17 @@ fn test_line_buffer_insert_text_with_initially_empty_data() {
 	assert line_buf.lines == ["1"]
 }
 
+fn test_line_buffer_insert_text_at_offset_higher_than_current_line_size() {
+	mut line_buf := LineBuffer{
+		lines: ["1. first line", "2. second line", "3. third line", "4. fourth line"]
+	}
+
+	new_pos := line_buf.insert_text(Position.new(1, 79), " text at end of second line")?
+	assert new_pos == Position.new(1, 41)
+	assert line_buf.lines == ["1. first line", "2. second line text at end of second line", "3. third line", "4. fourth line"]
+}
+
+
 fn test_line_buffer_insert_text_at_line_higher_than_current_data() {
 	mut line_buf := LineBuffer{
 		lines: ["1. first line", "2. second line", "3. third line", "4. fourth line"]
