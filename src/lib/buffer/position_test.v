f9a6ad9ce9cc06ebb9f41e959231365d2a8a0990
tauraamui <adampstringer@protonmail.com>
2025-07-04 20:21:34 +0100

    test: adjust addition behaviour to match expected

--- src/lib/buffer/position_test.v
+++ src/lib/buffer/position_test.v
@@ -26,7 +26,7 @@ fn test_add_with_some_line_distance() {
 	mut pos := Position{ line: 1, offset: 3 }
 	dist    := Distance{ lines: 12, offset: 4 }
 
-	assert pos.add(dist) == Position{ line: 13, offset: 4 }
+	assert pos.add(dist) == Position{ line: 13, offset: 7 }
 }
 
 fn test_less_than_works_when_position_a_less_then_b() {
