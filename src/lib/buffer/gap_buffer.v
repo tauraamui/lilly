9666354bfe20054118ee85b2f34d31da8dfb048d
tauraamui <adampstringer@protonmail.com>
2025-07-08 16:43:28 +0100

    fix: always remove gap size from resolved offset

--- src/lib/buffer/gap_buffer.v
+++ src/lib/buffer/gap_buffer.v
@@ -420,10 +420,7 @@ pub fn (gap_buffer GapBuffer) up_to_next_blank_line(pos Pos) ?Pos {
 	mut cursor_loc := pos
 	cursor_loc.x = 0
 	mut offset := gap_buffer.find_offset(Position.new(cursor_loc.y, cursor_loc.x)) or { return none }
-
-	if offset > gap_buffer.gap_end {
-		offset -= gap_buffer.gap_end - gap_buffer.gap_start
-	}
+	offset -= gap_buffer.gap_end - gap_buffer.gap_start
 
 	// NOTE(tauraamui) [10/01/25]:
 	// this copying of the two sides of the buffer might be worth
@@ -438,14 +435,13 @@ pub fn (gap_buffer GapBuffer) up_to_next_blank_line(pos Pos) ?Pos {
 	data_post_gap := gap_buffer.data[gap_buffer.gap_end..]
 	data := arrays.merge(data_pre_gap, data_post_gap)
 
-	offset -= gap_size
 	mut newline_count := 0
 	mut last_rune_was_newline := false
 	for i := offset; i >= 0; i-- {
 		c := data[i]
 		if c == lf {
-			cursor_loc.y -= 1
 			if last_rune_was_newline { return cursor_loc }
+			cursor_loc.y -= 1
 			last_rune_was_newline = true
 			continue
 		}
