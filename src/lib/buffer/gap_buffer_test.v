7064f79d2a21a9b07527ee5cb1108c2c1399e973
tauraamui <adampstringer@protonmail.com>
2025-07-08 16:36:45 +0100

    test: gap buffer doesn't jump up if newline below cursor

--- src/lib/buffer/gap_buffer_test.v
+++ src/lib/buffer/gap_buffer_test.v
@@ -20,6 +20,12 @@ fn test_up_to_next_blank_line_in_document_with_no_blank_line_given_cursor_at_top
 	assert new_pos == none
 }
 
+fn test_up_to_next_blank_line_in_document_with_blank_line_below_the_cursor_at_top() {
+	mut gb := GapBuffer.new("1. First line\n\n2. Second line!\n3. Third line :3")
+	new_pos := gb.up_to_next_blank_line(Pos{ x: 0, y: 0 })
+	assert new_pos == none
+}
+
 fn test_inserting_into_gap_buffer() {
 	mut gb := GapBuffer.new("12345")
 
