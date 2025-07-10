5df7569b425088d1540edc1d5ba6b0901e5d371f
tauraamui <adampstringer@protonmail.com>
2025-07-10 15:31:26 +0100

    tidy: remove redundant old code

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -468,34 +468,6 @@ pub fn (buffer Buffer) up_to_next_blank_line(pos Pos) ?Pos {
 	return none
 }
 
-pub fn (buffer Buffer) up_to_next_blank_line1(pos Pos) ?Pos {
-	if buffer.use_gap_buffer {
-		return buffer.c_buffer.up_to_next_blank_line(pos)
-	}
-	mut cursor := pos
-	cursor = buffer.clamp_cursor_within_document_bounds(pos)
-	if cursor.y == 0 { return none }
-
-	if buffer.lines.len == 0 { return none }
-
-	mut compound_y := 0
-	for i := cursor.y; i >= 0; i-- {
-		if i == cursor.y { continue }
-		compound_y += 1
-		if buffer.lines[i].len == 0 {
-			break
-		}
-	}
-
-	if compound_y > 0 {
-		cursor.x = 0
-		cursor.y -= compound_y
-		return cursor
-	}
-
-	return none
-}
-
 pub fn (buffer Buffer) down_to_next_blank_line(pos Pos) ?Pos {
 	if buffer.use_gap_buffer {
 		return buffer.c_buffer.down_to_next_blank_line(pos)
