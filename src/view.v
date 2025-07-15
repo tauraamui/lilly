1001961e8809ebdf118539c20e6a32015402d262
tauraamui <adampstringer@protonmail.com>
2025-07-15 17:34:00 +0100

    refactor: improve shape of the code for existing visual indent methods

--- src/view.v
+++ src/view.v
@@ -681,9 +681,9 @@ fn (mut view View) insert_tab() {
 //                             thought through before this stuff is migrated to the
 //                             buffer wrapper type data structure.
 fn (mut view View) visual_indent() {
-	mut start := if sel_start := view.cursor.sel_start() { sel_start.y } else { return
+	start := if ss := view.cursor.sel_start() { ss.y } else { return
 	 }
-	mut end := if sel_end := view.cursor.sel_end() { sel_end.y } else { return
+	end := if ss := view.cursor.sel_end() { ss.y } else { return
 	 }
 
 	prefix := if view.config.insert_tabs_not_spaces { '\t' } else { ' '.repeat(4) }
@@ -694,9 +694,9 @@ fn (mut view View) visual_indent() {
 }
 
 fn (mut view View) visual_unindent() {
-	mut start := if sel_start := view.cursor.sel_start() { sel_start.y } else { return
+	start := if ss := view.cursor.sel_start() { ss.y } else { return
 	 }
-	mut end := if sel_end := view.cursor.sel_end() { sel_end.y } else { return
+	end := if ss := view.cursor.sel_end() { ss.y } else { return
 	 }
 
 	prefix := if view.config.insert_tabs_not_spaces { '\t' } else { ' '.repeat(4) }
