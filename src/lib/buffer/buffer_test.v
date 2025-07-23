8652b841253990a6265c96e4256c1bf6b78a7807
tauraamui <adampstringer@protonmail.com>
2025-07-23 23:57:34 +0100

    test: validate that existing buffer enter does nothing for line buffer

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -231,6 +231,16 @@ fn test_buffer_legacy_buffer_enter_inserts_newline_line() {
 	assert buffer.str() == '1. f\nirst line\n2. second line\n3. third line'
 }
 
+fn test_buffer_line_buffer_enter_inserts_newline_line() {
+	mut buffer := Buffer.new('', .line_buffer)
+	buffer.load_contents_into_line_buffer(['1. first line', '2. second line', '3. third line'])
+	buffer.enter(Pos{ x: 4, y: 0 })
+	// NOTE(tauraamui) [23/07/2025]: currently the line buffer version of this method
+	//                               does nothing, and is therefore not expected to mutate
+	//                               the document by inserting any newlines
+	assert buffer.str() == '1. first line\n2. second line\n3. third line'
+}
+
 fn test_buffer_gap_buffer_x_deletes_char_from_line() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
