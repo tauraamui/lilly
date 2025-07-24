a722b53ea80ef2cca70d555cb29e3573caaf620b
tauraamui <adampstringer@protonmail.com>
2025-07-24 21:40:11 +0100

    test: wip changing call sites of method to use new type in and out

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -317,18 +317,12 @@ fn test_buffer_legacy_buffer_x_deletes_char_from_line() {
 	mut buffer := Buffer.new('', .legacy)
 	buffer.lines = ['1. first line', '2. second line', '3. third line']
 
-	mut new_pos := buffer.x(Pos{ x: 4, y: 0 })?
-	assert new_pos == Pos{
-		x: 4
-		y: 0
-	}
+	mut new_pos := buffer.x(Position.new( line: 0, offset: 4))?
+	assert new_pos == Position.new(line: 0, offset: 4)
 	assert buffer.str() == '1. frst line\n2. second line\n3. third line'
 
 	new_pos = buffer.x(new_pos)?
-	assert new_pos == Pos{
-		x: 4
-		y: 0
-	}
+	assert new_pos == Position.new(line: 0, offset: 4)
 	assert buffer.str() == '1. fst line\n2. second line\n3. third line'
 }
 
@@ -337,18 +331,13 @@ fn test_buffer_line_buffer_x_deletes_char_from_line() {
 	lines := ['1. first line', '2. second line', '3. third line']
 	buffer.load_contents_into_line_buffer(lines)
 
-	mut new_pos := buffer.x(Pos{ x: 4, y: 0 })?
-	assert new_pos == Pos{
-		x: 4
-		y: 0
-	}
+	mut new_pos := buffer.x(Position.new(line: 0, offset: 4))?
+	assert new_pos == Position.new(line: 0, offset: 4)
+
 	assert buffer.str() == '1. frst line\n2. second line\n3. third line'
 
 	new_pos = buffer.x(new_pos)?
-	assert new_pos == Pos{
-		x: 4
-		y: 0
-	}
+	assert new_pos == Position.new(line: 0, offset: 4)
 	assert buffer.str() == '1. fst line\n2. second line\n3. third line'
 }
 
