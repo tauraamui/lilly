ce2e8871b78b3583286067d9f91990629160d7a9
tauraamui <adampstringer@protonmail.com>
2025-07-26 22:13:59 +0100

    test: ensure existing backspace method works for gap buffer

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -411,6 +411,56 @@ fn test_buffer_legacy_buffer_o_inserts_newline() {
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
+fn test_buffer_gap_buffer_backspace_deletes_char_from_line() {
+	mut buffer := Buffer.new('', .gap_buffer)
+	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
+
+	mut new_pos := buffer.backspace(Pos{ x: 4, y: 0 })?
+	assert new_pos == Pos{
+		x: 3
+		y: 0
+	}
+
+	assert buffer.str() == '1. irst line\n2. second line\n3. third line'
+
+	new_pos = buffer.backspace(new_pos)?
+	assert new_pos == Pos{
+		x: 2
+		y: 0
+	}
+	assert buffer.str() == '1.irst line\n2. second line\n3. third line'
+}
+
+/*
+fn test_buffer_legacy_buffer_x_deletes_char_from_line() {
+	mut buffer := Buffer.new('', .legacy)
+	buffer.lines = ['1. first line', '2. second line', '3. third line']
+
+	mut new_pos := buffer.x(Position.new(line: 0, offset: 4))?
+	assert new_pos == Position.new(line: 0, offset: 4)
+
+	assert buffer.str() == '1. frst line\n2. second line\n3. third line'
+
+	new_pos = buffer.x(new_pos)?
+	assert new_pos == Position.new(line: 0, offset: 4)
+	assert buffer.str() == '1. fst line\n2. second line\n3. third line'
+}
+
+fn test_buffer_line_buffer_x_deletes_char_from_line() {
+	mut buffer := Buffer.new('', .line_buffer)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.load_contents_into_line_buffer(lines)
+
+	mut new_pos := buffer.x(Position.new(line: 0, offset: 4))?
+	assert new_pos == Position.new(line: 0, offset: 4)
+	assert buffer.str() == '1. frst line\n2. second line\n3. third line'
+
+	new_pos = buffer.x(new_pos)?
+	assert new_pos == Position.new(line: 0, offset: 4)
+	assert buffer.str() == '1. fst line\n2. second line\n3. third line'
+}
+*/
+
 fn test_buffer_line_buffer_o_inserts_newline() {
 	mut buffer := Buffer.new('', .line_buffer)
 	lines := ['1. first line', '2. second line', '3. third line']
