5b465fe7caf91e183ef78dd1fcdda5f657c9f9c9
tauraamui <adampstringer@protonmail.com>
2025-07-26 23:34:08 +0100

    test: define line buffer base for backspace method for buffer wrap type

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -451,21 +451,31 @@ fn test_buffer_legacy_buffer_backspace_deletes_char_from_line() {
 	assert buffer.str() == '1.irst line\n2. second line\n3. third line'
 }
 
-/*
-fn test_buffer_line_buffer_x_deletes_char_from_line() {
+// NOTE(tauraamui) [26/07/2025]: line buffer backspace is currently a noop,
+//                               this is currently intended behaviour, so the
+//                               test for now should check for nothing at all
+fn test_buffer_line_buffer_backspace_deletes_char_from_line() {
 	mut buffer := Buffer.new('', .line_buffer)
 	lines := ['1. first line', '2. second line', '3. third line']
 	buffer.load_contents_into_line_buffer(lines)
 
-	mut new_pos := buffer.x(Position.new(line: 0, offset: 4))?
-	assert new_pos == Position.new(line: 0, offset: 4)
-	assert buffer.str() == '1. frst line\n2. second line\n3. third line'
+	/*
+	mut new_pos := buffer.backspace(Pos{ x: 4, y: 0 })?
+	assert new_pos == Pos{
+		x: 3
+		y: 0
+	}
 
-	new_pos = buffer.x(new_pos)?
-	assert new_pos == Position.new(line: 0, offset: 4)
-	assert buffer.str() == '1. fst line\n2. second line\n3. third line'
+	assert buffer.str() == '1. irst line\n2. second line\n3. third line'
+
+	new_pos = buffer.backspace(new_pos)?
+	assert new_pos == Pos{
+		x: 2
+		y: 0
+	}
+	assert buffer.str() == '1.irst line\n2. second line\n3. third line'
+	*/
 }
-*/
 
 fn test_buffer_line_buffer_o_inserts_newline() {
 	mut buffer := Buffer.new('', .line_buffer)
