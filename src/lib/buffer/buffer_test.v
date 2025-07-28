959c31dfa62bb9120c364ee35ee00cd741ae4d1d
tauraamui <adampstringer@protonmail.com>
2025-07-28 12:35:38 +0100

    test: duplicate tests to check new buffer o method

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -390,6 +390,44 @@ fn test_buffer_line_buffer_delete_does_nothing() {
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
+fn test_buffer_gap_buffer_o_new_inserts_newline() {
+	mut buffer := Buffer.new('', .gap_buffer)
+	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
+
+	new_pos := buffer.o_new(Pos{ x: 3, y: 1 })?
+	assert new_pos == Pos{
+		x: 0
+		y: 2
+	}
+	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
+}
+
+fn test_buffer_legacy_buffer_o_new_inserts_newline() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.lines = lines
+
+	new_pos := buffer.o_new(Pos{ x: 3, y: 1 })?
+	assert new_pos == Pos{
+		x: 0
+		y: 2
+	}
+	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
+}
+
+fn test_buffer_line_buffer_o_new_inserts_newline() {
+	mut buffer := Buffer.new('', .line_buffer)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.load_contents_into_line_buffer(lines)
+
+	new_pos := buffer.o_new(Pos{ x: 3, y: 1 })?
+	assert new_pos == Pos{
+		x: 0
+		y: 2
+	}
+	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
+}
+
 fn test_buffer_gap_buffer_o_inserts_newline() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
@@ -402,6 +440,7 @@ fn test_buffer_gap_buffer_o_inserts_newline() {
 	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
 }
 
+// TODO(tauraamui) [28/07/2025]: this test seems to be testing the wrong thing, check this out
 fn test_buffer_legacy_buffer_o_inserts_newline() {
 	mut buffer := Buffer.new('', .legacy)
 	lines := ['1. first line', '2. second line', '3. third line']
