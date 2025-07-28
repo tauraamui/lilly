22d8e7dd785cc7d881abda1ea3a7c5bb204d55d0
tauraamui <adampstringer@protonmail.com>
2025-07-28 12:31:08 +0100

    refactor: slightly re-arrange layout positioning of existing test

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -411,6 +411,19 @@ fn test_buffer_legacy_buffer_o_inserts_newline() {
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
+fn test_buffer_line_buffer_o_inserts_newline() {
+	mut buffer := Buffer.new('', .line_buffer)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.load_contents_into_line_buffer(lines)
+
+	new_pos := buffer.o(Pos{ x: 3, y: 1 })?
+	assert new_pos == Pos{
+		x: 0
+		y: 2
+	}
+	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
+}
+
 fn test_buffer_gap_buffer_backspace_deletes_char_from_line() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
@@ -463,19 +476,6 @@ fn test_buffer_line_buffer_backspace_deletes_char_from_line() {
 	}
 	assert buffer.str() == '1.irst line\n2. second line\n3. third line'
 	*/
-}
-
-fn test_buffer_line_buffer_o_inserts_newline() {
-	mut buffer := Buffer.new('', .line_buffer)
-	lines := ['1. first line', '2. second line', '3. third line']
-	buffer.load_contents_into_line_buffer(lines)
-
-	new_pos := buffer.o(Pos{ x: 3, y: 1 })?
-	assert new_pos == Pos{
-		x: 0
-		y: 2
-	}
-	assert buffer.str() == '1. first line\n2. second line\n\n3. third line'
 }
 
 fn test_buffer_gap_buffer_left_moves_cursor_left_successfully() {
