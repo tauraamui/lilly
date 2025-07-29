8e5129bfa2f372de702035f102dd1894891baf48
tauraamui <adampstringer@protonmail.com>
2025-07-29 02:07:22 +0100

    chore: left new method to template replace position to new type

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -458,6 +458,23 @@ pub fn (buffer Buffer) find_prev_word_start(pos Pos) ?Pos {
 
 // *
 
+pub fn (buffer Buffer) left_new(pos Pos, insert_mode bool) ?Pos {
+	match buffer.buffer_kind {
+		.gap_buffer {
+			return buffer.c_buffer.left(pos)
+		}
+		.line_buffer {
+			return position_to_pos(buffer.l_buffer.left(Position.new(line: pos.y, offset: pos.x)))
+		}
+		.legacy {
+			mut cursor := pos
+			cursor.x -= 1
+			cursor = buffer.clamp_cursor_x_pos(cursor, insert_mode)
+			return cursor
+		}
+	}
+}
+
 pub fn (buffer Buffer) left(pos Pos, insert_mode bool) ?Pos {
 	match buffer.buffer_kind {
 		.gap_buffer {
