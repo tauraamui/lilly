553e5e1c7a494aae36650102dc75a36ab52d40dd
Adam Stringer <adampstringer@protonmail.com>
2025-07-29 14:43:11 +0100

    LIL-482/migrate buffer left method to use new position type (#510)
    * chore: left new method to template replace position to new type
    
    * test: define new left method tests
    
    * chore: update left new method to use new position type
    
    * test: ensure new left method works correctly per buffer type
    
    * chore: update position type at buffer left method definition
    
    * test: remove additional now unneeded tests
    
    * chore: change callsite to pass in new position type for left method
    
    * fix: if gap buffer left fails return original position
    
    * style: auto-format V code in src/

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -477,11 +477,8 @@ fn test_buffer_gap_buffer_left_moves_cursor_left_successfully() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
 
-	new_pos := buffer.left(Pos{ x: 3, y: 1 }, false)?
-	assert new_pos == Pos{
-		x: 2
-		y: 1
-	}
+	new_pos := buffer.left(Position.new(line: 1, offset: 3), false)?
+	assert new_pos == Position.new(line: 1, offset: 2)
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
@@ -490,11 +487,8 @@ fn test_buffer_legacy_buffer_left_moves_cursor_left_successfully() {
 	lines := ['1. first line', '2. second line', '3. third line']
 	buffer.lines = lines
 
-	new_pos := buffer.left(Pos{ x: 2, y: 1 }, false)?
-	assert new_pos == Pos{
-		x: 1
-		y: 1
-	}
+	new_pos := buffer.left(Position.new(line: 1, offset: 3), false)?
+	assert new_pos == Position.new(line: 1, offset: 2)
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
@@ -503,11 +497,8 @@ fn test_buffer_line_buffer_left_moves_cursor_left_successfully() {
 	lines := ['1. first line', '2. second line', '3. third line']
 	buffer.load_contents_into_line_buffer(lines)
 
-	new_pos := buffer.left(Pos{ x: 3, y: 1 }, false)?
-	assert new_pos == Pos{
-		x: 2
-		y: 1
-	}
+	new_pos := buffer.left(Position.new(line: 1, offset: 3), false)?
+	assert new_pos == Position.new(line: 1, offset: 2)
 	assert buffer.str() == '1. first line\n2. second line\n3. third line'
 }
 
