21793321fed2c75dc33eb3598e6c10750d429c79
tauraamui <adampstringer@protonmail.com>
2025-07-29 02:07:29 +0100

    test: define new left method tests

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -473,6 +473,44 @@ fn test_buffer_line_buffer_backspace_deletes_char_from_line() {
 	*/
 }
 
+fn test_buffer_gap_buffer_left_new_moves_cursor_left_successfully() {
+	mut buffer := Buffer.new('', .gap_buffer)
+	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
+
+	new_pos := buffer.left_new(Pos{ x: 3, y: 1 }, false)?
+	assert new_pos == Pos{
+		x: 2
+		y: 1
+	}
+	assert buffer.str() == '1. first line\n2. second line\n3. third line'
+}
+
+fn test_buffer_legacy_buffer_left_new_moves_cursor_left_successfully() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.lines = lines
+
+	new_pos := buffer.left_new(Pos{ x: 2, y: 1 }, false)?
+	assert new_pos == Pos{
+		x: 1
+		y: 1
+	}
+	assert buffer.str() == '1. first line\n2. second line\n3. third line'
+}
+
+fn test_buffer_line_buffer_left_new_moves_cursor_left_successfully() {
+	mut buffer := Buffer.new('', .line_buffer)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.load_contents_into_line_buffer(lines)
+
+	new_pos := buffer.left_new(Pos{ x: 3, y: 1 }, false)?
+	assert new_pos == Pos{
+		x: 2
+		y: 1
+	}
+	assert buffer.str() == '1. first line\n2. second line\n3. third line'
+}
+
 fn test_buffer_gap_buffer_left_moves_cursor_left_successfully() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
