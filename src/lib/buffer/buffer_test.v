ea48034a02614f5463e1df55ddc34cd4c48a504c
tauraamui <adampstringer@protonmail.com>
2025-08-03 14:48:49 +0100

    chore: duplicate clamp method and tests

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -740,6 +740,60 @@ fn test_buffer_gap_buffer_find_end_of_line() {
 	assert end_of_line_pos_x == 11
 }
 
+fn test_buffer_clamp_cursor_pos_x_within_line_new_not_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 8, y: 0 }, false) == Pos{ x: 8, y: 0 }
+}
+
+fn test_buffer_clamp_cursor_pos_x_longer_than_line_new_not_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 20, y: 0 }, false) == Pos{ x: 12, y: 0 }
+}
+
+fn test_buffer_clamp_cursor_pos_x_same_length_of_line_new_not_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 12, y: 0 }, true) == Pos{ x: 12, y: 0 }
+}
+
+fn test_buffer_clamp_cursor_pos_x_within_line_new_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 8, y: 0 }, true) == Pos{ x: 8, y: 0 }
+}
+
+fn test_buffer_clamp_cursor_pos_x_longer_than_line_new_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 20, y: 0 }, true) == Pos{ x: 13, y: 0 }
+}
+
+fn test_buffer_clamp_cursor_pos_x_same_length_of_line_new_insert_mode() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
+		'5. fifth line']
+	buffer.lines = lines
+
+	assert buffer.clamp_cursor_x_pos_new(Pos{ x: 13, y: 0 }, true) == Pos{ x: 13, y: 0 }
+}
+
 fn test_buffer_clamp_cursor_pos_x_within_line_not_insert_mode() {
 	mut buffer := Buffer.new('', .legacy)
 	lines := ['This is a doc', '', '1. first line', '2. second line', '3. third line', '',
