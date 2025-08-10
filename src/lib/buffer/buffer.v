bda801aa0b27acba46caab756d9087417d4b0599
tauraamui <adampstringer@protonmail.com>
2025-08-10 16:12:00 +0100

    chore: update callsite and buffer method to use new position type

--- src/lib/buffer/buffer.v
+++ src/lib/buffer/buffer.v
@@ -458,8 +458,12 @@ pub fn (buffer Buffer) find_next_word_end(pos Position) ?Position {
 	}
 }
 
-pub fn (buffer Buffer) find_prev_word_start(pos Pos) ?Pos {
-	return buffer.c_buffer.find_prev_word_start(pos)
+pub fn (buffer Buffer) find_prev_word_start(pos Position) ?Position {
+	return if p := buffer.c_buffer.find_prev_word_start(position_to_pos(pos)) {
+		pos_to_position(p)
+	} else {
+		pos
+	}
 }
 
 // *
