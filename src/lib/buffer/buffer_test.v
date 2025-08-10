a655dafe647fc8b957ef0e20a6b8b5326b457de1
tauraamui <adampstringer@protonmail.com>
2025-08-10 16:42:48 +0100

    test: ensure new replace char method works correctly

--- src/lib/buffer/buffer_test.v
+++ src/lib/buffer/buffer_test.v
@@ -469,6 +469,20 @@ fn test_replace_char() {
 	assert buffer.str() == '1. firsq line\n2. second lpne\n3. third line'
 }
 
+fn test_replace_char_new() {
+	mut buffer := Buffer.new('', .legacy)
+	lines := ['1. first line', '2. second line', '3. third line']
+	buffer.lines = lines
+
+	assert buffer.str() == '1. first line\n2. second line\n3. third line'
+
+	buffer.replace_char_new(Position.new(line: 0, offset: 7), 113, "q")
+	assert buffer.str() == '1. firsq line\n2. second line\n3. third line'
+
+	buffer.replace_char_new(Position.new(line: 1, offset: 11), 112, "p")
+	assert buffer.str() == '1. firsq line\n2. second lpne\n3. third line'
+}
+
 fn test_buffer_gap_buffer_left_moves_cursor_left_successfully() {
 	mut buffer := Buffer.new('', .gap_buffer)
 	buffer.c_buffer = GapBuffer.new('1. first line\n2. second line\n3. third line')
