37316e693dfb13edfcfd9cc8559d93665dffe4bb
tauraamui <adampstringer@protonmail.com>
2025-09-23 17:34:18 +0100

    test(buffer): start to duplicate existing test with new version check

--- src/lib/buffer/gap_buffer_test.v
+++ src/lib/buffer/gap_buffer_test.v
@@ -58,20 +58,18 @@ fn test_read_range_of_each_full_line_from_document_with_content() {
 	assert gb.read(Range.new(start_pos, end_pos))? == '3. Third line :3'
 }
 
-/*
-fn test_up_to_next_blank_line_in_document_with_no_blank_line_given_cursor_at_top2() {
+fn test_up_to_next_blank_line2_in_document_with_no_blank_line_given_cursor_at_top() {
 	mut gb := GapBuffer.new('1. First line\n2. Second line!\n3. Third line :3')
-	new_pos := gb.up_to_next_blank_line2(Pos{ x: 0, y: 0 })
-	assert new_pos == none
+	assert gb.up_to_next_blank_line2(Position.new(line: 0, offset: 0)) == none
 }
 
-fn test_up_to_next_blank_line_in_document_with_blank_line_below_the_cursor_at_top2() {
+/*
+fn test_up_to_next_blank_line2_in_document_with_blank_line_below_the_cursor_at_top() {
 	mut gb := GapBuffer.new('1. First line\n\n2. Second line!\n3. Third line :3')
-	new_pos := gb.up_to_next_blank_line(Pos{ x: 0, y: 0 })
-	assert new_pos == none
+	assert gb.up_to_next_blank_line(Pos{ x: 0, y: 0 }) == none
 }
 
-fn test_up_to_next_blank_line_in_document_with_blank_line_above_the_cursor_in_middle2() {
+fn test_up_to_next_blank_line2_in_document_with_blank_line_above_the_cursor_in_middle() {
 	mut gb := GapBuffer.new('1. First line\n\n2. Second line!\n3. Third line :3')
 	assert gb.up_to_next_blank_line(Pos{ x: 0, y: 3 })? == Pos{
 		x: 0
