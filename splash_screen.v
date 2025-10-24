f892c9388d9185ba66bd31492b6efeb157d40d9c
tauraamui <adampstringer@protonmail.com>
2025-10-24 17:51:46 +0100

    feat(ui): implement splash screen model to use as initial state

--- splash_screen.v
+++ splash_screen.v
@@ -1,12 +1,26 @@
 module main
 
+import math
 import tauraamui.bobatea as tea
 
+const logo_contents = $embed_file('./splash-logo.txt')
+
+struct SplashLogo {
+mut:
+	data  []string
+	width int
+}
+
 struct SplashScreenModel {
+    logo SplashLogo
 }
 
 fn new_splash_screen_model() SplashScreenModel {
-    return SplashScreenModel{}
+    return SplashScreenModel{
+        logo: SplashLogo{
+            data: logo_contents.to_string().split_into_lines()
+        }
+    }
 }
 
 fn (mut m SplashScreenModel) init() ?tea.Cmd {
@@ -30,7 +44,45 @@ fn (mut m SplashScreenModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
 }
 
 fn (m SplashScreenModel) view(mut ctx tea.Context) {
-    ctx.draw_text(0, 0, "Lilly v2 (petal)")
+    render_logo(mut ctx, m.logo)
+}
+
+fn render_logo(mut ctx tea.Context, logo SplashLogo) {
+	offset_x := 0
+	mut offset_y := 0 + f64(ctx.window_height()) * 0.1
+	assert offset_y >= 1
+	ctx.set_color(r: 245, g: 191, b: 243)
+	for i, l in logo.data {
+		start_x := offset_x + (ctx.window_width() / 2) - (l.runes().len / 2)
+		assert start_x > 2
+		if has_colouring_directives(l) {
+			for j, c in l.runes() {
+				mut to_draw := '${c}'
+				if to_draw == 'g' {
+					to_draw = ' '
+					ctx.set_color(r: 97, g: 242, b: 136)
+				}
+				if to_draw == 'p' {
+					to_draw = ' '
+					ctx.set_color(r: 245, g: 191, b: 243)
+				}
+				ctx.draw_text(start_x + j, int(math.floor(offset_y)) + i, to_draw)
+			}
+			continue
+		}
+		ctx.draw_text(offset_x + (ctx.window_width() / 2) - (l.runes().len / 2),
+			int(math.floor(offset_y)) + i, l)
+	}
+	ctx.reset_color()
+}
+
+fn has_colouring_directives(line string) bool {
+	for c in line.split('') {
+		if c == 'g' || c == 'p' {
+			return true
+		}
+	}
+	return false
 }
 
 fn (m SplashScreenModel) clone() tea.Model {
