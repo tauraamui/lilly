55c8d6726a55f78d62594457170fc3f9faf8f1d5
tauraamui <adampstringer@protonmail.com>
2025-10-26 14:10:34 +0000

    ui: use new offset compact method to compartmentalise offsetting

--- splash_screen.v
+++ splash_screen.v
@@ -46,25 +46,29 @@ fn (mut m SplashScreenModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
 fn (m SplashScreenModel) view(mut ctx tea.Context) {
     defer { ctx.clear_offset() }
     // NOTE(tauraamui) [25/10/2025]: all following contents to be padded from top of window
-	base_offset_y := f64(ctx.window_height()) * 0.1
-    ctx.push_offset(tea.Offset{ y: int(math.floor(base_offset_y)) })
-    render_logo(mut ctx, m.logo)
-    render_version(mut ctx)
+    offset := render_logo(mut ctx, m.logo)
+    render_version(mut ctx, offset)
     render_keybinds_list(mut ctx)
 }
 
 fn render_keybinds_list(mut ctx tea.Context) {
 }
 
-fn render_version(mut ctx tea.Context) {
+fn render_version(mut ctx tea.Context, offset tea.Offset) {
+    defer { ctx.clear_offset() }
+    ctx.push_offset(offset)
     ctx.draw_text(0, 0, "lilly (project petal)")
 }
 
-fn render_logo(mut ctx tea.Context, logo SplashLogo) {
+fn render_logo(mut ctx tea.Context, logo SplashLogo) tea.Offset {
+    defer { ctx.clear_offset() }
+    // NOTE(tauraamui) [25/10/25]: this can be reduced to a style container which basically
+    //                  makes the y offset be down by 10% of the parent. in this
+    //                  case the parent is just the window itself, but could be anything
+	base_offset_y := f64(ctx.window_height()) * 0.1
+    ctx.push_offset(tea.Offset{ y: int(math.floor(base_offset_y)) })
+
 	offset_x := 0
-	mut offset_y := 0 + f64(ctx.window_height()) * 0.1
-	assert offset_y >= 1
-	ctx.push_offset(tea.Offset{ y: int(math.floor(offset_y)) })
 	ctx.set_color(r: 245, g: 191, b: 243)
 	for i, l in logo.data {
 		start_x := offset_x + (ctx.window_width() / 2) - (l.runes().len / 2)
@@ -87,6 +91,7 @@ fn render_logo(mut ctx tea.Context, logo SplashLogo) {
 		ctx.draw_text(offset_x + (ctx.window_width() / 2) - (l.runes().len / 2), i, l)
 	}
 	ctx.reset_color()
+	return ctx.compact_offsets()
 }
 
 fn has_colouring_directives(line string) bool {
