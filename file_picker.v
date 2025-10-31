e82722c4af662229df2131d76988558b4282de6b
tauraamui <adampstringer@protonmail.com>
2025-10-31 14:49:14 +0000

    feat(ui): render file picker as floating box with border

--- file_picker.v
+++ file_picker.v
@@ -3,6 +3,8 @@ module main
 import tauraamui.bobatea as tea
 
 struct FilePickerModel {
+	width int
+	height int
 }
 
 struct OpenDialogMsg {
@@ -13,7 +15,10 @@ struct CloseDialogMsg {}
 
 fn open_file_picker() tea.Msg {
 	return OpenDialogMsg{
-		model: FilePickerModel{}
+		model: FilePickerModel{
+			width: 80
+			height: 30
+		}
 	}
 }
 
@@ -39,11 +44,20 @@ fn (mut m FilePickerModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
 	return m.clone(), none
 }
 
+const bordered_layout = tea.new_layout()
+	.border(.normal)
+	.border_color(tea.Color.ansi(69))
+	.padding_all(1)
+
 fn (m FilePickerModel) view(mut ctx tea.Context) {
-	model_content := "FILE PICKER"
-	ctx.set_bg_color(tea.Color.ansi(55))
-	ctx.draw_text((ctx.window_width() / 2) - tea.visible_len(model_content) / 2, ctx.window_height() / 2, model_content)
-	ctx.reset_bg_color()
+	id := ctx.push_offset(tea.Offset{ x: (ctx.window_width() / 2) - m.width / 2, y: (ctx.window_height() / 2) - m.height / 2 })
+	defer { ctx.clear_offsets_from(id) }
+	bordered_layout.size(m.width, m.height).render(mut ctx, fn [m] (mut ctx tea.Context) {
+		model_content := "FILE PICKER"
+		ctx.set_bg_color(tea.Color.ansi(55))
+		ctx.draw_text(0, 0, model_content)
+		ctx.reset_bg_color()
+	})
 }
 
 fn (m FilePickerModel) clone() tea.Model {
