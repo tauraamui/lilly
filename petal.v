14e02ffc0df9ef245b9bc22c0bb33c74321fcfec
tauraamui <adampstringer@protonmail.com>
2025-11-01 19:10:44 +0000

    refactor(ui): remove active screen state tracking as boba handles this

--- petal.v
+++ petal.v
@@ -2,21 +2,14 @@ module main
 
 import tauraamui.bobatea as tea
 
-enum State as u8 {
-    root
-    splash_screen
-}
-
 struct PetalModel {
 mut:
-    state State
-    splash_screen SplashScreenModel
+    active_screen tea.Model
 }
 
 fn new_petal_model() PetalModel {
     return PetalModel{
-        state: .splash_screen
-        splash_screen: new_splash_screen_model()
+        active_screen: new_splash_screen_model()
     }
 }
 
@@ -25,40 +18,14 @@ fn (mut m PetalModel) init() ?tea.Cmd {
 }
 
 fn (mut m PetalModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
-	mut cmds := []tea.Cmd{}
-	match m.state {
-	    .splash_screen {
-	        s, cmd := m.splash_screen.update(msg)
-	        if s is SplashScreenModel {
-	            m.splash_screen = s
-	        }
-	        u_cmd := cmd or { tea.noop_cmd }
-			cmds << u_cmd
-	    }
-	    .root {
-	    	match msg {
-	    		tea.KeyMsg {
-					cmds << tea.quit
-	    		}
-	    		else {}
-	    	}
-	    }
-	}
-
-	return m.splash_screen.clone(), tea.batch_array(cmds)
+	screen, cmds := m.active_screen.update(msg)
+	m.active_screen = screen
+	return m.active_screen.clone(), cmds
 }
 
 fn (m PetalModel) view(mut ctx tea.Context) {
-    if m.state == .splash_screen {
-        m.splash_screen.view(mut ctx)
-        return
-    }
-    window_width := ctx.window_width()
-    window_height := ctx.window_height()
-
-    title := "Petal Text Editor"
-
-    ctx.draw_text((window_width / 2) - tea.visible_len(title) / 2, window_height / 2, title)
+    mut screen := m.active_screen
+    screen.view(mut ctx)
 }
 
 fn (m PetalModel) clone() tea.Model {
