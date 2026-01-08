7fc1f110c390b2d14da9db62768fd65397e23de6
tauraamui <adampstringer@protonmail.com>
2026-01-08 17:32:04 +0000

    chore: adjust field names to be more consistent with previous

--- theme.v
+++ theme.v
@@ -9,22 +9,22 @@ pub const light_theme_name = "light"
 pub struct Theme{
 pub:
 	name                         string    @[required]
-	background_color             tea.Color @[required]
-	foreground_color             tea.Color
+	bg_color                     tea.Color @[required]
+	fg_color                     tea.Color
 	active_split_divider_color   tea.Color @[required]
 	inactive_split_divider_color tea.Color @[required]
 }
 
 pub const dark_theme = Theme{
 	name: "dark"
-	background_color: palette.matte_black_bg_color
+	bg_color: palette.matte_black_bg_color
 	active_split_divider_color: palette.petal_pink_color
 	inactive_split_divider_color: palette.status_bar_bg_color
 }
 
 pub const light_theme = Theme{
 	name: "light"
-	background_color: palette.matte_white_fg_color
+	bg_color: palette.matte_white_fg_color
 	active_split_divider_color: palette.petal_pink_color
 	inactive_split_divider_color: palette.status_bar_bg_color
 }
