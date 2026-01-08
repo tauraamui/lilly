074de293171888026b7d4d05f22a84cd0edfc8d1
tauraamui <adampstringer@protonmail.com>
2026-01-08 17:22:10 +0000

    chore: define config type with useful constructor pattern

---
+++ config.v
@@ -0,0 +1,36 @@
+module cfg
+
+import theme
+
+pub struct Config{
+pub:
+	theme      theme.Theme
+	leader_key string
+}
+
+@[params]
+pub struct ConfigOptions {
+pub:
+	load_from_path ?string = "~/.config/petal/petal.cfg"
+}
+
+pub fn Config.new(opts ConfigOptions) Config {
+	if path_to_load := opts.load_from_path {
+		assert path_to_load.len > 0
+		// do some loading from file here
+		// return Config{}
+	}
+
+	return Config{
+		theme: theme.dark_theme
+		leader_key: ";"
+	}
+}
+
+pub fn (c Config) set_theme(name string) Config {
+	return Config{
+		...c
+		theme: if name == "dark" { theme.dark_theme } else { theme.light_theme }
+	}
+}
+
