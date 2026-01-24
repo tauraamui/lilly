8e508b9b96f8a0a8063796f3c2144bc402be9d89
tauraamui <adampstringer@protonmail.com>
2026-01-24 21:24:42 +0000

    test: ensure initialised gap buffer content loads correctly

--- gap_buffer_test.v
+++ gap_buffer_test.v
@@ -1,26 +1,14 @@
 module buffers
 
-@[assert_continues]
-fn test_initialise_gap_buffer_with_contents() {
-	tests := {
-		'': ''.runes()
-		'abc': 'abc'.runes()
-	}
-
-	for k, v in tests {
-		gp := GapBuffer.new(v)
-		assert gp.content() == k
-		assert gp.raw_content().map(fn (c rune) rune {
-			return if c == null_code_point { `_` } else { c }
-		}).string() == 'iwejfijewoifjweif'
-	}
-}
-
 fn test_initialise_gap_buffer_with_no_contents() {
 	gb := GapBuffer.new(''.runes())
 	assert gb.content() == ''
-	assert gb.raw_content().map(fn (c rune) rune {
-		return if c == null_code_point { `_` } else { c }
-	}).string() == `_`.repeat(initial_gap_size)
+	assert gb.raw_content().map(null_code_point_to_str).string() == `_`.repeat(initial_gap_size)
+}
+
+fn test_initialise_gap_buffer_with_content() {
+	gb := GapBuffer.new('abcdef'.runes())
+	assert gb.content() == 'abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == '${`_`.repeat(initial_gap_size)}abcdef'
 }
 
