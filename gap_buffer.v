ab958436fa3de232212d5bca14bcd4636f29eefb
tauraamui <adampstringer@protonmail.com>
2026-01-24 21:03:40 +0000

    chore: initial primative def for gap buffer

---
+++ gap_buffer.v
@@ -0,0 +1,28 @@
+module buffers
+
+pub struct GapBuffer {
+	data      []rune
+	gap_start u32
+	gap_size  u32
+}
+
+const null_code_point = rune(0xfeff)
+const initial_gap_size = 32
+
+pub fn GapBuffer.new(content []rune) GapBuffer {
+	mut gb := GapBuffer{
+		data: []rune{ len: content.len + initial_gap_size, init: null_code_point }
+		gap_start: 0
+		gap_size: initial_gap_size
+	}
+	gb.insert(content)
+	return gb
+}
+
+pub fn (mut g GapBuffer) insert(data []rune) {
+}
+
+pub fn (g GapBuffer) content() string {
+	return g.data[g.gap_start + g.gap_size..].string()
+}
+
