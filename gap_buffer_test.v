b6fd681aa008461b9f9041736afcbb81e7098267
tauraamui <adampstringer@protonmail.com>
2026-01-26 15:59:47 +0000

    test: gap grows consistently

--- gap_buffer_test.v
+++ gap_buffer_test.v
@@ -53,4 +53,35 @@ fn test_insert_char_into_gap_buffer_with_existing_content_overflow_gap_grows_gap
 	assert gb.raw_content().map(null_code_point_to_str).string() == 'z123__abcdef'
 }
 
+@[assert_continues]
+fn test_insert_char_into_gap_buffer_with_existing_content_overflow_gap_grows_gap_consistently() {
+	mut gb := GapBuffer.new(content: 'abcdef'.runes(), gap_size: 3)
+	gb.insert_char(`z`)
+	assert gb.content() == 'zabcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z__abcdef'
+
+	gb.insert_char(`1`)
+	assert gb.content() == 'z1abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z1_abcdef'
+
+	gb.insert_char(`2`)
+	assert gb.content() == 'z12abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z12abcdef'
+
+	gb.insert_char(`3`)
+	assert gb.content() == 'z123abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z123__abcdef'
+
+	gb.insert_char(`4`)
+	assert gb.content() == 'z1234abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z1234_abcdef'
+
+	gb.insert_char(`5`)
+	assert gb.content() == 'z12345abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z12345abcdef'
+
+	gb.insert_char(`6`)
+	assert gb.content() == 'z123456abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z123456__abcdef'
+}
 
