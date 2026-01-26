72aff5b488346c581b144a05dc5efc939205170e
tauraamui <adampstringer@protonmail.com>
2026-01-26 01:35:17 +0000

    test: gap grows when empty

--- gap_buffer_test.v
+++ gap_buffer_test.v
@@ -3,27 +3,27 @@ module buffers
 fn test_initialise_gap_buffer_with_no_contents() {
 	gb := GapBuffer.new(content: ''.runes())
 	assert gb.content() == ''
-	assert gb.raw_content().map(null_code_point_to_str).string() == `_`.repeat(initial_gap_size)
+	assert gb.raw_content().map(null_code_point_to_str).string() == `_`.repeat(int(initial_gap_size))
 }
 
 fn test_initialise_gap_buffer_with_content() {
 	gb := GapBuffer.new(content: 'abcdef'.runes())
 	assert gb.content() == 'abcdef'
-	assert gb.raw_content().map(null_code_point_to_str).string() == '${`_`.repeat(initial_gap_size)}abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == '${`_`.repeat(int(initial_gap_size))}abcdef'
 }
 
 fn test_insert_char_into_gap_buffer_with_no_existing_content() {
 	mut gb := GapBuffer.new(content: ''.runes())
 	gb.insert_char(`z`)
 	assert gb.content() == 'z'
-	assert gb.raw_content().map(null_code_point_to_str).string() == 'z${`_`.repeat(initial_gap_size - 1)}'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z${`_`.repeat(int(initial_gap_size - 1))}'
 }
 
 fn test_insert_char_into_gap_buffer_with_existing_content() {
 	mut gb := GapBuffer.new(content: 'abcdef'.runes())
 	gb.insert_char(`z`)
 	assert gb.content() == 'zabcdef'
-	assert gb.raw_content().map(null_code_point_to_str).string() == 'z${`_`.repeat(initial_gap_size - 1)}abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z${`_`.repeat(int(initial_gap_size - 1))}abcdef'
 }
 
 fn test_insert_char_into_gap_buffer_with_existing_content_with_custom_gap_size() {
@@ -31,6 +31,26 @@ fn test_insert_char_into_gap_buffer_with_existing_content_with_custom_gap_size()
 	gb.insert_char(`z`)
 	assert gb.content() == 'zabcdef'
 	assert gb.raw_content().map(null_code_point_to_str).string() == 'z__abcdef'
+}
+
+@[assert_continues]
+fn test_insert_char_into_gap_buffer_with_existing_content_overflow_gap_grows_gap() {
+	mut gb := GapBuffer.new(content: 'abcdef'.runes(), gap_size: 3)
+	gb.insert_char(`z`)
+	assert gb.content() == 'zabcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z__abcdef'
+
+	gb.insert_char(`1`)
+	assert gb.content() == 'z1abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z1_abcdef'
+
+	gb.insert_char(`2`)
+	assert gb.content() == 'z12abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z12abcdef'
+
+	gb.insert_char(`3`)
+	assert gb.content() == 'z123abcdef'
+	assert gb.raw_content().map(null_code_point_to_str).string() == 'z123__abcdef'
 }
 
 
