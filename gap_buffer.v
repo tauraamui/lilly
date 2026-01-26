0e58dedac6a90284ac728ae27194aa77ac2b8dd5
tauraamui <adampstringer@protonmail.com>
2026-01-26 01:35:08 +0000

    feat: internally expand gap when necessary

--- gap_buffer.v
+++ gap_buffer.v
@@ -1,6 +1,9 @@
 module buffers
 
+import arrays
+
 pub struct GapBuffer {
+	initial_gap_size u32
 mut:
 	data      []rune
 	gap_start u32
@@ -8,16 +11,17 @@ mut:
 }
 
 const null_code_point = rune(0xfeff)
-const initial_gap_size = 32
+const initial_gap_size = u32(32)
 
 @[params]
 pub struct GapBufferParams {
-	content []rune
-	gap_size        u32 = initial_gap_size
+	content  []rune
+	gap_size u32 = initial_gap_size
 }
 
 pub fn GapBuffer.new(opts GapBufferParams) GapBuffer {
 	mut gb := GapBuffer{
+		initial_gap_size: opts.gap_size
 		data: []rune{ len: opts.content.len + int(opts.gap_size), init: null_code_point }
 		gap_start: 0
 		gap_end: opts.gap_size
@@ -26,13 +30,29 @@ pub fn GapBuffer.new(opts GapBufferParams) GapBuffer {
 	return gb
 }
 
-pub fn (mut g GapBuffer) initial_fill(data []rune) {
+fn (mut g GapBuffer) initial_fill(data []rune) {
 	for i, c in data {
 		g.data[int(g.gap_end) + i] = c
 	}
 }
 
+fn (mut g GapBuffer) grow_gap() {
+	mut dest := []rune{ len: g.data.len + int(g.initial_gap_size), init: null_code_point }
+	arrays.copy(mut dest[..g.gap_start], g.data[..g.gap_start])
+	gap_end := g.gap_start + g.initial_gap_size
+	arrays.copy(mut dest[gap_end..], g.data[g.gap_end..])
+	g.gap_end = gap_end
+	g.data = dest
+}
+
+fn (g GapBuffer) current_gap_size() u32 {
+	return u32(g.gap_end - g.gap_start)
+}
+
 pub fn (mut g GapBuffer) insert_char(data rune) {
+	if g.current_gap_size() == 0 {
+		g.grow_gap()
+	}
 	g.data[g.gap_start] = data
 	g.gap_start += 1
 }
