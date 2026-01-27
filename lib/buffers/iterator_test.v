f6c3f8b9cbfabc04e97d03773a560807599f855f
tauraamui <adampstringer@protonmail.com>
2026-01-27 00:55:16 +0000

    test: line iterator handles sequence of newlines correctly

--- lib/buffers/iterator_test.v
+++ lib/buffers/iterator_test.v
@@ -10,3 +10,15 @@ fn test_data_iterator_basic_contiguous_lines() {
 	assert iter.next()  == none
 }
 
+fn test_data_iterator_multiple_blank_lines() {
+	data := [rune(`a`), `b`, `c`, `\n`, `\n`, `\n`, `d`, `e`, `f`, `\n`, `g`, `h`, `i`]
+	mut iter := LineIterator.new(data)
+
+	assert iter.next()? == [rune(`a`), `b`, `c`]
+	assert iter.next()? == []
+	assert iter.next()? == []
+	assert iter.next()? == [rune(`d`), `e`, `f`]
+	assert iter.next()? == [rune(`g`), `h`, `i`]
+	assert iter.next()  == none
+}
+
