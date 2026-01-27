9ffb393406750a6109c39a0d882ebfd5c316c19d
tauraamui <adampstringer@protonmail.com>
2026-01-27 00:52:07 +0000

    test: iterator handles basic sequence of lines correctly

--- lib/buffers/iterator_test.v
+++ lib/buffers/iterator_test.v
@@ -1,10 +1,12 @@
 module buffers
 
-fn test_data_iterator() {
-	data := [rune(`a`), `b`, `c`, `\n`, `d`, `e`, `f`]
+fn test_data_iterator_basic_contiguous_lines() {
+	data := [rune(`a`), `b`, `c`, `\n`, `d`, `e`, `f`, `\n`, `g`, `h`, `i`]
 	mut iter := LineIterator.new(data)
 
 	assert iter.next()? == [rune(`a`), `b`, `c`]
 	assert iter.next()? == [rune(`d`), `e`, `f`]
+	assert iter.next()? == [rune(`g`), `h`, `i`]
+	assert iter.next()  == none
 }
 
