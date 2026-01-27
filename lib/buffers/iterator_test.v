183f6e28a6ad731d499439cb93145fc957556990
tauraamui <adampstringer@protonmail.com>
2026-01-27 00:59:09 +0000

    test: iterator handles multiple and single trailing + leading newlines

--- lib/buffers/iterator_test.v
+++ lib/buffers/iterator_test.v
@@ -22,3 +22,31 @@ fn test_data_iterator_multiple_blank_lines() {
 	assert iter.next()  == none
 }
 
+fn test_data_iterator_leading_and_trailing_single_blank_lines() {
+	data := [rune(`\n`), `a`, `b`, `c`, `\n`, `\n`, `\n`, `d`, `e`, `f`, `\n`, `g`, `h`, `i`, `\n`]
+	mut iter := LineIterator.new(data)
+
+	assert iter.next()? == []
+	assert iter.next()? == [rune(`a`), `b`, `c`]
+	assert iter.next()? == []
+	assert iter.next()? == []
+	assert iter.next()? == [rune(`d`), `e`, `f`]
+	assert iter.next()? == [rune(`g`), `h`, `i`]
+	assert iter.next()  == none
+}
+
+fn test_data_iterator_leading_and_trailing_multiple_blank_lines() {
+	data := [rune(`\n`), `\n`, `a`, `b`, `c`, `\n`, `\n`, `\n`, `d`, `e`, `f`, `\n`, `g`, `h`, `i`, `\n`, `\n`]
+	mut iter := LineIterator.new(data)
+
+	assert iter.next()? == []
+	assert iter.next()? == []
+	assert iter.next()? == [rune(`a`), `b`, `c`]
+	assert iter.next()? == []
+	assert iter.next()? == []
+	assert iter.next()? == [rune(`d`), `e`, `f`]
+	assert iter.next()? == [rune(`g`), `h`, `i`]
+	assert iter.next()? == []
+	assert iter.next()  == none
+}
+
