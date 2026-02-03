2c4689c0a81a33194baae30ee7a23c309a4f61d8
tauraamui <adampstringer@protonmail.com>
2026-02-03 15:59:08 +0000

    test: ensure cursor movement works

---
+++ editor_test.v
@@ -0,0 +1,32 @@
+module main
+
+fn test_cursor_up_moves_up() {
+	mut cursor := ModelCursorPos{}
+	assert cursor.x == 0
+	assert cursor.y == 0
+
+	cursor = cursor.left()
+	assert cursor.x == 0
+	assert cursor.y == 0
+
+	cursor = cursor.down(max_height: 100)
+	assert cursor.x == 0
+	assert cursor.y == 1
+
+	cursor = cursor.up()
+	assert cursor.x == 0
+	assert cursor.y == 0
+
+	cursor = cursor.right(max_width: 100)
+	assert cursor.x == 1
+	assert cursor.y == 0
+
+	cursor = cursor.down(max_height: 100)
+	assert cursor.x == 0
+	assert cursor.y == 1
+
+	cursor = cursor.right(distance: 99, max_width: 100)
+	assert cursor.x == 99
+	assert cursor.y == 1
+}
+
