945928ee1f989dd224cbeaeaaecba7f0e1348f30
tauraamui <adampstringer@protonmail.com>
2026-02-28 20:34:28 +0000

    test: ensure cursor pos jumps to next non-whitespace span

--- document_test.v
+++ document_test.v
@@ -3,7 +3,11 @@ module documents_test
 /* TEST BLOCK WHICH SHOULD NEVER CHANGE
 // This is a fake test comment for verifying word jumping
 fn random_function(a int, b int) int {
-	return a + b
+	a_sum := a * a
+
+
+	b_sum := b * b
+	return a_sum + b_sum
 }
 */
 
@@ -22,12 +26,31 @@ fn test_move_cursor_to_next_word_start() {
 	ctrl.move_cursor_right(meta_doc_id, .normal)
 	ctrl.move_cursor_right(meta_doc_id, .normal)
 
-	current_line := ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
+	mut current_line := ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
 
 	word_start_chars := ['i', 'a', 'f', 't', 'c', 'f', 'v', 'w', 'j']
 	for c in word_start_chars {
 		ctrl.move_cursor_to_next_word_start(meta_doc_id)
 		assert '${current_line.runes()[ctrl.cursor_pos(meta_doc_id).x]}' == c
 	}
+
+	ctrl.move_cursor_to_next_word_start(meta_doc_id)
+	current_line = ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
+	assert '${current_line.runes()[ctrl.cursor_pos(meta_doc_id).x]}' == 'f'
+
+	ctrl.move_cursor_to_next_word_start(meta_doc_id)
+	current_line = ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
+	assert '${current_line.runes()[ctrl.cursor_pos(meta_doc_id).x]}' == 'r'
+
+	ctrl.move_cursor_down(meta_doc_id, .normal)
+	ctrl.move_cursor_down(meta_doc_id, .normal)
+
+	ctrl.move_cursor_to_next_word_start(meta_doc_id)
+	current_line = ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
+	assert '${current_line.runes()[ctrl.cursor_pos(meta_doc_id).x]}' == 'b'
+
+	ctrl.move_cursor_to_next_word_start(meta_doc_id)
+	current_line = ctrl.get_line_at(meta_doc_id, ctrl.cursor_pos(meta_doc_id).y) or { panic('failed to aquire current line') }
+	assert '${current_line.runes()[ctrl.cursor_pos(meta_doc_id).x]}' == ':'
 }
 
