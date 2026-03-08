ff5ac433a84ef292cdeef038ef97b20d499d375d
tauraamui <adampstringer@protonmail.com>
2026-03-08 00:46:15 +0000

    feat: implement jump to previous blank line

--- document.v
+++ document.v
@@ -266,10 +266,15 @@ fn (d Document) move_cursor_to_line_end(pos CursorPos, mode petal.Mode) CursorPo
 fn (d Document) move_cursor_to_next_blank_line(pos CursorPos) CursorPos {
 	// NOTE(tauraamui) [08/03/2026]: for now we don't care about iterating previous lines
 	// skipping them should be fast enough even though it is a waste to retrieve them
+	mut last_y := pos.y
 	for i, line in d.data.iter() {
+		last_y = i
 		if i > pos.y {
 			if line.len == 0 { return CursorPos{ y: i } }
 		}
+	}
+	if last_y > pos.y {
+		return CursorPos{ y: last_y }
 	}
 	return pos
 }
@@ -278,10 +283,18 @@ fn (d Document) move_cursor_to_previous_blank_line(pos CursorPos) CursorPos {
 	// NOTE(tauraamui) [08/03/2026]: for now we don't care about iterating previous lines
 	// skipping them should be fast enough even though it is a waste to retrieve them
 	// needs to use reverse iterator?
+	mut last_blank := -1
 	for i, line in d.data.iter() {
-		if i > pos.y {
-			if line.len == 0 { return CursorPos{ y: i } }
+		if i >= pos.y { break }
+		if line.len == 0 {
+			last_blank = i
 		}
+	}
+	if last_blank >= 0 {
+		return CursorPos{ y: last_blank }
+	}
+	if pos.y > 0 {
+		return CursorPos{ y: 0 }
 	}
 	return pos
 }
