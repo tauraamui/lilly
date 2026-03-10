3a2a396bb4cdcafeab4b8e05fe484b5b092db81b
tauraamui <adampstringer@protonmail.com>
2026-03-10 18:05:16 +0000

    test: ensure position xy set methods work correctly

--- lib/documents/cursor/cursor_test.v
+++ lib/documents/cursor/cursor_test.v
@@ -11,3 +11,15 @@ fn test_position_equality_ignores_largest_x() {
 	y_pos := cursor.Pos.new(1, 2).x(3).x(9)
 	assert x_pos == y_pos
 }
+
+fn test_position_set_x() {
+	p := cursor.Pos.new(8, 5)
+	assert p.x == 8
+	assert p.y == 5
+}
+
+fn test_position_set_y() {
+	p := cursor.Pos.new(11, 3)
+	assert p.x == 11
+	assert p.y == 3
+}
