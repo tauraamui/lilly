b4b48bd9e99084f62733a1742a7c08dadf91729e
tauraamui <adampstringer@protonmail.com>
2026-03-10 17:56:37 +0000

    test: add additional asserts for pos type to ensure in place comp only

--- lib/documents/cursor/cursor_test.v
+++ lib/documents/cursor/cursor_test.v
@@ -6,4 +6,8 @@ fn test_position_equality_ignores_largest_x() {
 	a_pos := cursor.Pos.new(10, 10)
 	b_pos := cursor.Pos.new(10, 10)
 	assert a_pos == b_pos
+
+	x_pos := cursor.Pos.new(1, 2).x(120).x(9)
+	y_pos := cursor.Pos.new(1, 2).x(3).x(9)
+	assert x_pos == y_pos
 }
