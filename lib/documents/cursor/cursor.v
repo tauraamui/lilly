6997c0b06a9e2e78ed8b389a0ce8de383e6bb3b7
tauraamui <adampstringer@protonmail.com>
2026-03-10 17:50:40 +0000

    chore: define new cursor position type to aspire to migrate to

---
+++ lib/documents/cursor/cursor.v
@@ -0,0 +1,34 @@
+module cursor
+
+@[noinit]
+pub struct Pos {
+	largest_x int // used to track largest x set across struct instance lifetime
+pub:
+	x int
+	y int
+}
+
+pub fn Pos.new(x int, y int) Pos {
+	return Pos{ x: x, y: y, largest_x: x }
+}
+
+fn (a Pos) == (b Pos) bool {
+	return a.x == b.x && a.y == b.y
+}
+
+pub fn (p Pos) x(x int) Pos {
+	xx := if x < 0 { 0 } else { x }
+	return Pos{
+		x: xx
+		y: p.y
+		largest_x: if xx > p.largest_x { xx } else { p.largest_x }
+	}
+}
+
+pub fn (p Pos) y(y int) Pos {
+	return Pos{
+		x: p.x
+		y: y
+		largest_x: p.largest_x
+	}
+}
