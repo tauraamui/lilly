059a1f2c1856a20b3b7480bded9173aca77b38ce
tauraamui <adampstringer@protonmail.com>
2026-06-02 20:16:03 +0100

    test: ensure insertions of single bytes works correctly

--- lib/buffers/gap/buffer_test.v
+++ lib/buffers/gap/buffer_test.v
@@ -1,6 +1,21 @@
 module gap
 
+const test_gap_size = 1024 / 2
+
 fn test_gap_init() {
-	assert true
+	assert Buffer.new(test_gap_size).buf.len == test_gap_size
+}
+
+fn test_insert_into_gap_sets_slot_and_increments_cursor() {
+	mut gb := Buffer.new(test_gap_size)
+	gb.insert(u8(`c`))
+	assert gb.buf[gb.ccur - 1] == u8(`c`)
+	assert gb.ccur == 1
+
+	gb.insert(u8(`d`))
+	assert gb.buf[gb.ccur - 1] == u8(`d`)
+	assert gb.ccur == 2
+
+	assert gb.str() == 'cd'
 }
 
