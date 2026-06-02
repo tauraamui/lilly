f8ff1c6e911679a8850f6ed9ae72db382ca38396
tauraamui <adampstringer@protonmail.com>
2026-06-02 21:15:15 +0100

    test: ensure grow and cursor move left works

--- lib/buffers/gap/buffer_test.v
+++ lib/buffers/gap/buffer_test.v
@@ -1,9 +1,24 @@
 module gap
 
-const test_gap_size = 1024 / 2
+const test_gap_size = 8
 
 fn test_gap_init() {
 	assert Buffer.new(test_gap_size).buf.len == test_gap_size
+}
+
+fn test_move_cursor_left() {
+	mut gb := Buffer.new(test_gap_size)
+	gb.insert(u8(`c`))
+	assert gb.buf[gb.ccur - 1] == u8(`c`)
+	assert gb.ccur == 1
+
+	gb.move_cur_left()
+	assert gb.ccur == 0
+	assert gb.cend == u64(test_gap_size - 1)
+
+	gb.move_cur_left()
+	assert gb.ccur == 0
+	assert gb.cend == u64(test_gap_size - 1)
 }
 
 fn test_insert_into_gap_sets_slot_and_increments_cursor() {
@@ -17,5 +32,54 @@ fn test_insert_into_gap_sets_slot_and_increments_cursor() {
 	assert gb.ccur == 2
 
 	assert gb.str() == 'cd'
+	assert gb.rawstr() == 'cd______'
+}
+
+fn test_insert_beyond_gap_clamped() {
+	mut gb := Buffer.new(8)
+	gb.insert(u8(`c`))
+	assert gb.buf[gb.ccur - 1] == u8(`c`)
+	assert gb.ccur == 1
+	assert gb.rawstr() == 'c_______'
+
+	gb.insert(u8(`d`))
+	assert gb.buf[gb.ccur - 1] == u8(`d`)
+	assert gb.ccur == 2
+	assert gb.rawstr() == 'cd______'
+
+	gb.insert(u8(`e`))
+	assert gb.buf[gb.ccur - 1] == u8(`e`)
+	assert gb.ccur == 3
+	assert gb.rawstr() == 'cde_____'
+
+	gb.insert(u8(`f`))
+	assert gb.buf[gb.ccur - 1] == u8(`f`)
+	assert gb.ccur == 4
+	assert gb.rawstr() == 'cdef____'
+
+	gb.insert(u8(`g`))
+	assert gb.buf[gb.ccur - 1] == u8(`g`)
+	assert gb.ccur == 5
+	assert gb.rawstr() == 'cdefg___'
+
+	gb.insert(u8(`h`))
+	assert gb.buf[gb.ccur - 1] == u8(`h`)
+	assert gb.ccur == 6
+	assert gb.rawstr() == 'cdefgh__'
+
+	gb.insert(u8(`i`))
+	assert gb.buf[gb.ccur - 1] == u8(`i`)
+	assert gb.ccur == 7
+	assert gb.rawstr() == 'cdefghi_'
+
+	gb.insert(u8(`j`))
+	assert gb.buf[gb.ccur - 1] == u8(`j`)
+	assert gb.ccur == 8
+	assert gb.rawstr() == 'cdefghij'
+
+	gb.insert(u8(`k`))
+	assert gb.buf[gb.ccur - 1] == u8(`k`)
+	assert gb.ccur == 9
+	assert gb.rawstr() == 'cdefghijk_______'
 }
 
---
+++ lib/buffers/gap_buf_test.v
@@ -0,0 +1,51 @@
+module buffers
+
+import gap
+
+fn test_insert_into_gap_results_in_expected_string() {
+	mut b := gap.Buffer.new(512)
+	b.insert(u8(`c`))
+	assert b.str() == 'c'
+}
+
+@[assert_continues]
+fn test_insert_into_gap_then_move_cursor_left() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	assert b.str() == 'c'
+	assert b.rawstr() == 'c_______'
+
+	b.move_cur_left()
+	assert b.str() == 'c'
+	assert b.rawstr() == '_______c'
+}
+
+@[assert_continues]
+fn test_insert_into_gap_then_move_cursor_left_multiple_times() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	assert b.str() == 'c'
+	assert b.rawstr() == 'c_______'
+
+	b.move_cur_left()
+	assert b.str() == 'c'
+	assert b.rawstr() == '_______c'
+
+	b.move_cur_left()
+	assert b.str() == 'c'
+	assert b.rawstr() == '_______c'
+}
+
+@[assert_continues]
+fn test_insert_into_gap_then_move_cursor_left_insert_char() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	assert b.str() == 'c'
+	assert b.rawstr() == 'c_______'
+
+	b.move_cur_left()
+	b.insert(u8(`b`))
+	assert b.str() == 'bc'
+	assert b.rawstr() == 'b______c'
+}
+
