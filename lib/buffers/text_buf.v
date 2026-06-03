33229226cccf558e8c23137153f3bccf6bed0a82
tauraamui <adampstringer@protonmail.com>
2026-06-03 20:12:21 +0100

    feat: basic method for acquiring specific lines bytes

--- lib/buffers/text_buf.v
+++ lib/buffers/text_buf.v
@@ -16,5 +16,27 @@ pub fn TextBuffer.new() TextBuffer { // TODO(tauraamui) [2026-06-03]: pass in re
 	}
 }
 
+pub fn (mut tb TextBuffer) insert(c u8) {
+	tb.data_buf.insert(c)
+}
+
+pub fn (tb TextBuffer) get_line_bytes(y u64) []u8 {
+	line_start, line_end := tb.get_line_start_and_end(y)
+	mut line_bytes := []u8{ len: int(line_start + line_end) }
+	mut c := 0
+	for li in line_start..line_end {
+		if c_byte := tb.data_buf.get(li) {
+			line_bytes[c] = c_byte
+		}
+		c += 1
+	}
+	return line_bytes
+}
+
+fn (tb TextBuffer) get_line_start_and_end(y u64) (u64, u64) {
+	line_start := tb.line_buf.offsets[y]
+	line_end := if y + 1 < tb.line_buf.offsets.len { tb.line_buf.offsets[y + 1] } else { tb.data_buf.logical_len() }
+	return line_start, line_end
+}
 
 
