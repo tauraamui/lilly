29500658c01d5feb017232c33f74e24921b0b202
tauraamui <adampstringer@protonmail.com>
2026-06-03 20:47:46 +0100

    test: ensure get line bytes returns correct data per line

--- lib/buffers/text_buf_test.v
+++ lib/buffers/text_buf_test.v
@@ -16,4 +16,22 @@ fn test_text_buffer_get_line_bytes() {
 	assert tb.get_line_bytes(0) == [u8(`H`), `e`, `l`, `l`, `o`]
 }
 
+fn test_text_buffer_get_line_bytes_of_multiple_lines() {
+	mut tb := TextBuffer.new()
+	tb.insert(u8(`H`))
+	tb.insert(u8(`e`))
+	tb.insert(u8(`l`))
+	tb.insert(u8(`l`))
+	tb.insert(u8(`o`))
+	tb.insert(u8(`\n`))
+	tb.insert(u8(`W`))
+	tb.insert(u8(`o`))
+	tb.insert(u8(`r`))
+	tb.insert(u8(`l`))
+	tb.insert(u8(`d`))
+
+	assert tb.get_line_bytes(0) == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
+	assert tb.get_line_bytes(1) == [u8(`W`), `o`, `r`, `l`, `d`]
+}
+
 
