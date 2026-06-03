c337550994d9d5d7fd60977f4cd8efa0482820ab
tauraamui <adampstringer@protonmail.com>
2026-06-03 21:00:11 +0100

    test: ensure text buf backspace works correctly

--- lib/buffers/text_buf_test.v
+++ lib/buffers/text_buf_test.v
@@ -13,7 +13,7 @@ fn test_text_buffer_get_line_bytes() {
 	tb.insert(u8(`l`))
 	tb.insert(u8(`o`))
 
-	assert tb.get_line_bytes(0) == [u8(`H`), `e`, `l`, `l`, `o`]
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`]
 }
 
 fn test_text_buffer_get_line_bytes_of_multiple_lines() {
@@ -30,8 +30,35 @@ fn test_text_buffer_get_line_bytes_of_multiple_lines() {
 	tb.insert(u8(`l`))
 	tb.insert(u8(`d`))
 
-	assert tb.get_line_bytes(0) == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
-	assert tb.get_line_bytes(1) == [u8(`W`), `o`, `r`, `l`, `d`]
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
+	assert tb.get_line_bytes(1)? == [u8(`W`), `o`, `r`, `l`, `d`]
+}
+
+fn test_text_buffer_get_line_bytes_of_multiple_lines_post_backspace() {
+	mut tb := TextBuffer.new()
+	tb.insert(u8(`H`))
+	tb.insert(u8(`e`))
+	tb.insert(u8(`l`))
+	tb.insert(u8(`l`))
+	tb.insert(u8(`o`))
+	tb.insert(u8(`\n`))
+	tb.insert(u8(`W`))
+	tb.insert(u8(`o`))
+
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
+	assert tb.get_line_bytes(1)? == [u8(`W`), `o`]
+
+	tb.backspace()
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
+	assert tb.get_line_bytes(1)? == [u8(`W`)]
+
+	tb.backspace()
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`, `\n`]
+	assert tb.get_line_bytes(1)? == []
+
+	tb.backspace()
+	assert tb.get_line_bytes(0)? == [u8(`H`), `e`, `l`, `l`, `o`]
+	assert tb.get_line_bytes(1) == ?[]u8(none)
 }
 
 
