9618cd237ca57cefbc87e9c2d4546d4b8c54e737
tauraamui <adampstringer@protonmail.com>
2026-06-03 01:22:23 +0100

    test: ensure moving cursor backspace and delete all work

--- lib/buffers/gap_buf_test.v
+++ lib/buffers/gap_buf_test.v
@@ -8,6 +8,44 @@ fn test_insert_into_gap_results_in_expected_string() {
 	assert b.str() == 'c'
 }
 
+fn test_insert_into_gap_then_delete_results_in_expected_string() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	b.insert(u8(`d`))
+	b.insert(u8(`e`))
+	b.insert(u8(`f`))
+	assert b.str() == 'cdef'
+	assert b.rawstr() == 'cdef____'
+
+	b.move_cur_left()
+	b.move_cur_left()
+	assert b.str() == 'cdef'
+	assert b.rawstr() == 'cd____ef'
+
+	b.delete()
+	assert b.str() == 'cdf'
+	assert b.rawstr() == 'cd____ef'
+}
+
+fn test_insert_into_gap_then_backspace_results_in_expected_string() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	b.insert(u8(`d`))
+	b.insert(u8(`e`))
+	b.insert(u8(`f`))
+	assert b.str() == 'cdef'
+	assert b.rawstr() == 'cdef____'
+
+	b.move_cur_left()
+	b.move_cur_left()
+	assert b.str() == 'cdef'
+	assert b.rawstr() == 'cd____ef'
+
+	b.backspace()
+	assert b.str() == 'cef'
+	assert b.rawstr() == 'cd____ef'
+}
+
 @[assert_continues]
 fn test_insert_into_gap_then_move_cursor_left() {
 	mut b := gap.Buffer.new(8)
@@ -18,6 +56,27 @@ fn test_insert_into_gap_then_move_cursor_left() {
 	b.move_cur_left()
 	assert b.str() == 'c'
 	assert b.rawstr() == '_______c'
+}
+
+@[assert_continues]
+fn test_insert_into_gap_then_move_cursor_right() {
+	mut b := gap.Buffer.new(8)
+	b.insert(u8(`c`))
+	assert b.str() == 'c'
+	assert b.rawstr() == 'c_______'
+
+	b.move_cur_left()
+	assert b.str() == 'c'
+	assert b.rawstr() == '_______c'
+
+	b.insert(u8(`d`))
+	assert b.str() == 'dc'
+	assert b.rawstr() == 'd______c'
+
+	b.move_cur_right()
+	b.insert(u8(`z`))
+	assert b.str() == 'dcz'
+	assert b.rawstr() == 'dcz_____'
 }
 
 @[assert_continues]
