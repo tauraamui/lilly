ae4dc87777c0a772986798788c52827b7d086671
tauraamui <adampstringer@protonmail.com>
2026-06-17 17:25:35 +0100

    feat: render experimental split visualiser app bounds

--- experiment/muxer_tree.v
+++ experiment/muxer_tree.v
@@ -2,20 +2,40 @@ module main
 
 import bobatea as tea
 
+struct Node {
+	id int
+}
+
 @[noinit]
 struct AppModel {
+mut:
+	x         int
+	y         int
+	width     int
+	height    int
+	root_node Node
 }
 
 fn AppModel.new() AppModel {
-	return AppModel{}
+	return AppModel{
+		root_node: Node{ id: 1 }
+	}
 }
 
 fn (mut m AppModel) init() fn () tea.Msg {
-	return tea.noop_cmd
+	return tea.emit_resize
 }
+
+const padding = 4
 
 fn (mut m AppModel) update(msg tea.Msg) (tea.Model, fn () tea.Msg) {
 	match msg {
+		tea.ResizedMsg {
+			m.x = padding / 2
+			m.y = padding / 2
+			m.width = msg.window_width - padding
+			m.height = msg.window_height - padding
+		}
 		tea.KeyMsg {
 			match msg.k_type {
 				.special {
@@ -35,8 +55,35 @@ fn (mut m AppModel) update(msg tea.Msg) (tea.Model, fn () tea.Msg) {
 }
 
 fn (m AppModel) view(mut ctx tea.Context) {
-	split_tree_msg := 'split tree experiment render test'
-	ctx.draw_text((ctx.window_width() / 2) - (tea.visible_len(split_tree_msg) / 2), ctx.window_height() / 2, split_tree_msg)
+	// split_tree_msg := 'split tree experiment render test'
+	// ctx.draw_text((ctx.window_width() / 2) - (tea.visible_len(split_tree_msg) / 2), ctx.window_height() / 2, split_tree_msg)
+	m.draw_app_border(mut ctx)
+	render_node(mut ctx, m.root_node)
+}
+
+const box_top_left_corner     = [u8(0xe2), 0x94, 0x8c]
+const box_top_right_corner    = [u8(0xe2), 0x94, 0x90]
+const box_bottom_right_corner = [u8(0xe2), 0x94, 0x98]
+const box_bottom_left_corner  = [u8(0xe2), 0x94, 0x94]
+const box_horizontal          = [u8(0xe2), 0x94, 0x80]
+const box_vertical            = [u8(0xe2), 0x94, 0x82]
+
+fn (m AppModel) draw_app_border(mut ctx tea.Context) {
+	ctx.draw_text(m.x, m.y, '${box_top_left_corner.bytestr()}')
+	ctx.set_stroke('${box_horizontal.bytestr()}')
+	ctx.draw_line(m.x + 1, m.y, m.width, m.y, false)
+	ctx.set_stroke('${box_vertical.bytestr()}')
+	ctx.draw_line(m.x, m.y + 1, m.x, m.height, false)
+	ctx.draw_text(m.x + m.width - 1, m.y, '${box_top_right_corner.bytestr()}')
+	ctx.set_stroke('${box_vertical.bytestr()}')
+	ctx.draw_line(m.x + m.width - 1, m.y + 1, m.x + m.width - 1, m.height, false)
+	ctx.draw_text(m.x + m.width - 1, m.height + 1, '${box_bottom_right_corner.bytestr()}')
+	ctx.set_stroke('${box_horizontal.bytestr()}')
+	ctx.draw_line(m.x + 1, m.height + 1, m.x + m.width - 2, m.height + 1, false)
+	ctx.draw_text(m.x, m.height + 1, '${box_bottom_left_corner.bytestr()}')
+}
+
+fn render_node(mut ctx tea.Context, node Node) {
 }
 
 fn (m AppModel) clone() tea.Model {
